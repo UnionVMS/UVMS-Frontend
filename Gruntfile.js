54d82259bdbd7edbf86a5202412fec9273af7d6f
gusper <gusper@mycompany.com>
2015-04-08 09:07:10 +0000

    Updated gruntfile to NOT replace restConstants on grunt serve.
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/unionvms-web@618 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- Gruntfile.js
+++ Gruntfile.js
@@ -288,7 +288,7 @@ module.exports = function (grunt) {
   grunt.registerTask('test',['dom_munger:read', 'karma:all_tests', 'clean:after']);
 
   grunt.registerTask('default',['build-dev']);
-  grunt.registerTask('serve', ['replace:local', 'copy:serve', 'dom_munger:read','jshint','connect', 'watch']);
+  grunt.registerTask('serve', ['copy:serve', 'dom_munger:read','jshint','connect', 'watch']);
 
     grunt.event.on('watch', function(action, filepath) {
         //https://github.com/gruntjs/grunt-contrib-watch/issues/156
