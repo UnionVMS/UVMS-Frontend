545f5a6789c4e5f18d5a102b8777ac4d3801d0e5
emperssgu <emperssgu@mycompany.com>
2015-04-17 13:14:48 +0000

    [UVMS-551] Updated mobileTerminal list with pagination.
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/unionvms-web@696 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- app/partial/mobileTerminal/mobileTerminal.js
+++ app/partial/mobileTerminal/mobileTerminal.js
@@ -2,11 +2,19 @@ angular.module('unionvmsWeb').controller('MobileTerminalCtrl',function($scope, s
 
     $scope.currentSearchResults = new MobileTerminalListPage();
 
+    //Keep track of visibility statuses
     $scope.isVisible = {
         search : true,
         addNewMobileTerminal : false,
         viewMobileTerminal : false
     };
+
+    //Search objects and results
+    $scope.currentSearchResults = {
+        page : 1,
+        totalNumberOfPages : 1,
+        mobileTerminals : []
+    };    
 
     $scope.editSelectionDropdownItems =[{'text':'Poll terminals','code':'POLL'}, {'text':'Export selection','code':'EXPORT'}];
     $scope.transponderSystems = [];
@@ -37,7 +45,7 @@ angular.module('unionvmsWeb').controller('MobileTerminalCtrl',function($scope, s
 
     //Init function when entering page
     var init = function(){
-        //Load list with vessels
+        //Load list with mobileTerminals
         var response = searchService.searchMobileTerminals()
             .then(updateSearchResults, onGetSearchResultsError);
 
@@ -57,12 +65,47 @@ angular.module('unionvmsWeb').controller('MobileTerminalCtrl',function($scope, s
         );
     }; 
 
+    //Update the search results
     var updateSearchResults = function(mobileTerminalListPage){
-        $scope.currentSearchResults = mobileTerminalListPage.mobileTerminals;
+        $scope.mobileTerminalListPage = mobileTerminalListPage;
+        if(mobileTerminalListPage.mobileTerminals.length === 0 ){
+            $scope.error = "No mobile terminals matching you search criteria was found.";
+        } else {
+            $scope.error = "";
+            if(!$scope.currentSearchResults.mobileTerminals){
+                $scope.currentSearchResults.mobileTerminals = mobileTerminalListPage.mobileTerminals;
+            }
+            else {
+                for (var i = 0; i < mobileTerminalListPage.mobileTerminals.length; i++){
+                    $scope.currentSearchResults.mobileTerminals.push(mobileTerminalListPage.mobileTerminals[i]);
+                }
+            }
+        }
+        //Update page info
+        $scope.currentSearchResults.totalNumberOfPages = mobileTerminalListPage.totalNumberOfPages;
+        $scope.currentSearchResults.page = mobileTerminalListPage.currentPage;
+    }; 
+
+
+    //Load the next page of the search results
+    $scope.loadNextPage = function(){
+
+        if($scope.currentSearchResults.page < $scope.currentSearchResults.totalNumberOfPages )
+        {
+            //Increase page by 1
+            searchService.increasePage();
+        var response = searchService.searchMobileTerminals()
+            .then(updateSearchResults, onGetSearchResultsError);
+        }
     };
 
+
     var onGetSearchResultsError = function(error){
-        console.log(error);
+        $scope.error = "We are sorry... Something took a wrong turn. To err is human but to arr is pirate!!";
+        console.log("We are sorry... To err is human but to arr is pirate!!");
+
+        $scope.currentSearchResults.totalNumberOfPages = 1;
+        $scope.currentSearchResults.page = 1;
     };
 
     $scope.$on("$destroy", function() {
