d2a9f456ffd283ec32d5bb379b36f3af52ec8e3a
emperssgu <emperssgu@mycompany.com>
2015-04-21 11:44:22 +0000

    @Review Marcus, Gustav [UVMS-665] Bug fix missing texts from language files. Now the language files are loaded from the start.
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/unionvms-web@725 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- app/app.js
+++ app/app.js
@@ -1,15 +1,33 @@
 angular.module('unionvmsWeb', ['ui.bootstrap','ui.utils','ngRoute','ngAnimate','ngResource', 'ngLocalize']);
 
+//Resolve used for all routes
+var generalRouteResolves =  {
+    languages: function(initService){
+        return initService.loadLanguageFiles();
+    }
+};
+
 angular.module('unionvmsWeb').config(function($routeProvider) {
     $routeProvider
-        .when('/today',{templateUrl:'partial/today/today.html'})
-        .when('/vessel',{templateUrl:'partial/vessel/vessel.html'})
-        .when('/communication', {templateUrl: 'partial/mobileTerminal/mobileTerminal.html'});
+        .when('/today',{
+            templateUrl:'partial/today/today.html',
+            resolve: generalRouteResolves
+        })
+        .when('/vessel',{
+            templateUrl:'partial/vessel/vessel.html',
+            resolve: generalRouteResolves
+        })
+        .when('/communication', {
+            templateUrl: 'partial/mobileTerminal/mobileTerminal.html', 
+            resolve: generalRouteResolves
+        });
 
     /* Add New Routes Above */
     $routeProvider.otherwise({redirectTo:'/today'});
 
 });
+
+
 
 angular.module('unionvmsWeb').run(function($rootScope) {
 
@@ -36,4 +54,23 @@ angular.module('unionvmsWeb').value('localeConf', {
     cookieName: 'COOKIE_LOCALE_LANG',
     observableAttrs: new RegExp('^data-(?!ng-|i18n)'),
     delimiter: '::'
+});
+
+
+//Service used for bootstrapping the application
+angular.module('unionvmsWeb').factory('initService',function(locale) {
+
+    var initService = {
+        //Load the listed i18n files
+        loadLanguageFiles : function(){
+            return locale.ready([
+                'common',
+                'header',
+                'mobileTerminal',
+                'vessel'
+            ]);
+        },
+    };
+
+    return initService;
 });
