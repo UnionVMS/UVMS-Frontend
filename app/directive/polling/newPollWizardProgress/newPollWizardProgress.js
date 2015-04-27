655fea4044ed1f3e86f12948e4234da98e37f76d
emnilssol <emnilssol@mycompany.com>
2015-04-27 11:59:12 +0000

    @Review Ola, Gustav, Marcus
    * Steps directive takes now input, eg "step="1" etc..
    * Added words in langguage file
    
    
    
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/unionvms-web@819 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- app/directive/polling/newPollWizardProgress/newPollWizardProgress.js
+++ app/directive/polling/newPollWizardProgress/newPollWizardProgress.js
@@ -3,12 +3,14 @@ angular.module('unionvmsWeb').directive('newPollWizardProgress', function() {
 		restrict: 'E',
 		replace: true,
 		scope: {
-
+			step :'@'
 		},
 		templateUrl: 'directive/polling/newPollWizardProgress/newPollWizardProgress.html',
 		link: function(scope, element, attrs, fn) {
 
-
+			scope.isFirstStep = function(){return scope.step === "1";};
+			scope.isSecondStep = function(){return scope.step === "2";};
+			scope.isLastStep = function(){return scope.step === "3";};			
 		}
 	};
 });
