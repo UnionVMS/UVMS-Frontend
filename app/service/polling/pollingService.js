d4c3bf75ac7d8ef718639115b0b4d1450571bd97
emhedenma <emhedenma@mycompany.com>
2015-04-28 14:59:44 +0000

    @Review Ola, Marcus [UVMS-640] Adding terminals/groups to selection: * never adds the same terminals or group twice * does not add individual terminal if already selected as part of a group * removes individually selected terminals before adding their parent group
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/unionvms-web@853 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- app/service/polling/pollingService.js
+++ app/service/polling/pollingService.js
@@ -7,17 +7,61 @@ angular.module('unionvmsWeb').factory('pollingService',function() {
     };
 
     function indexOfObject(xs, x) {
-        for (var i = 0; i < xs.length; i++) {
-            if (xs[i] === x) {
-                return i;
+        if (typeof x === "function") {
+            // x = comparator function
+            for (var i = 0; i < xs.length; i++) {
+                if (x(xs[i])) {
+                    return i;
+                }
+            }
+        }
+        else {
+            // x = object
+            for (var i = 0; i < xs.length; i++) {
+                if (xs[i] === x) {
+                    return i;
+                }
             }
         }
 
         return -1;
     }
 
-    function removeTerminalGroup(groups, terminal) {
-        var idx = indexOfObject(groups, terminal);
+    function isSameTerminalGroup(group) {
+        return function(g) {
+            return g.name === group.name;
+        };
+    }
+
+    function containsTerminal(terminals, terminal) {
+        return indexOfObject(terminals, terminal) >= 0;
+    }
+
+    function containsTerminalGroup(groups, group) {
+        return indexOfObject(groups, isSameTerminalGroup(group)) >= 0;
+    }
+
+    function isTerminalSelected(terminal) {
+        if (containsTerminal(selection.selectedMobileTerminals, terminal)) {
+            return true;
+        }
+
+        for (var i = 0; i < selection.selectedMobileTerminalGroups.length; i++) {
+            var selectedGroup = selection.selectedMobileTerminalGroups[i];
+            if (containsTerminal(selectedGroup.mobileTerminals, terminal)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    function isTerminalGroupSelected(terminalGroup) {
+        return containsTerminalGroup(selection.selectedMobileTerminalGroups, terminalGroup);
+    }
+
+    function removeTerminalGroup(groups, group) {
+        var idx = indexOfObject(groups, isSameTerminalGroup(group));
         if (idx < 0) {
             return;
         }
@@ -39,13 +83,27 @@ angular.module('unionvmsWeb').factory('pollingService',function() {
     }
 
 	var pollingService = {
-        addMobileTerminalToSelection : function(terminal){
+        addMobileTerminalToSelection: function(terminal) {
+            if (isTerminalSelected(terminal)) {
+                return;
+            }
+
             selection.selectedMobileTerminals.push(terminal);
         },
-        addMobileTerminalGroupToSelection : function(terminalGroup){
+        addMobileTerminalGroupToSelection: function(terminalGroup) {
+            if (isTerminalGroupSelected(terminalGroup)) {
+                return;
+            }
+
+            // Remove any terminal in this group that was individually selected before
+            for (var i = 0; i < terminalGroup.mobileTerminals.length; i++) {
+                var terminal = terminalGroup.mobileTerminals[i]
+                removeTerminal(selection.selectedMobileTerminals, terminal);
+            }
+
             selection.selectedMobileTerminalGroups.push(terminalGroup);
         },
-        removeMobileTerminalFromSelection : function(terminalGroup, terminal) {
+        removeMobileTerminalFromSelection: function(terminalGroup, terminal) {
             if (terminalGroup) {
                 if (terminal) {
                     // Remove terminal in group
@@ -68,6 +126,8 @@ angular.module('unionvmsWeb').factory('pollingService',function() {
         getSelection : function(){
             return selection;
         },
+        isMobileTerminalSelected: isTerminalSelected,
+        isMobileTerminalGroupSelected: isTerminalGroupSelected
     };
 
 	return pollingService;
