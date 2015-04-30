dc4c22b74a02e39df211301b4929182b840ed2b3
emperssgu <emperssgu@mycompany.com>
2015-04-30 12:33:06 +0000

    @Review Marcus, Gustav Added cleaning up on polling page destroy.
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/unionvms-web@890 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- app/partial/polling/polling.js
+++ app/partial/polling/polling.js
@@ -1,5 +1,9 @@
-angular.module('unionvmsWeb').controller('PollingCtrl',function($scope){
+angular.module('unionvmsWeb').controller('PollingCtrl',function($scope, alertService, searchService){
 
     $scope.activeTab = "POLLING";
 
+    $scope.$on("$destroy", function() {
+        alertService.hideMessage();
+        searchService.reset();
+    });
 });
