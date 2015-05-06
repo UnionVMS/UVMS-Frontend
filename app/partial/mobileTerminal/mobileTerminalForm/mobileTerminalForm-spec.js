76d7935cf4eae144abdb9d5bdbc39b9f6a12b6a0
emperssgu <emperssgu@mycompany.com>
2015-05-06 10:32:25 +0000

    @Review Marcus, Gustav Added old test for create mobile terminal.
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/unionvms-web@979 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- app/partial/mobileTerminal/mobileTerminalForm/mobileTerminalForm-spec.js
+++ app/partial/mobileTerminal/mobileTerminalForm/mobileTerminalForm-spec.js
@@ -2,12 +2,16 @@ describe('mobileTerminalFormCtrl', function() {
 
 	beforeEach(module('unionvmsWeb'));
 
-	var scope,ctrl;
+	var scope,ctrl, createResponseTerminal;
 
     beforeEach(inject(function($rootScope, $controller, MobileTerminal) {
 		scope = $rootScope.$new();
 		ctrl = $controller('mobileTerminalFormCtrl', {$scope: scope});
 		scope.currentMobileTerminal = new MobileTerminal();
+
+        //Dummy response for create
+        createResponseTerminal = new MobileTerminal();
+        createResponseTerminal.mobileTerminalId.ids["INTERNAL_ID"] = "260";        
     }));
 
 	it('should unassign mobile terminal', inject(function($q, mobileTerminalRestService) {
@@ -17,4 +21,32 @@ describe('mobileTerminalFormCtrl', function() {
 		deferred.resolve();
 		expect(mobileTerminalRestService.unassignMobileTerminal).toHaveBeenCalled();
 	}));
+
+    it('should update currentMobileTerminal with created terminal', inject(function(MobileTerminal, $compile, $q, mobileTerminalRestService, alertService, locale) {
+        scope.currentMobileTerminal = new MobileTerminal();
+
+        scope.setCreateMode = function(bool){
+            scope.createNewMode = bool;
+        };
+
+        // A form to be valid
+        var element = angular.element('<form name="mobileTerminalForm"></form>');
+        $compile(element)(scope);
+
+        // Skip alert message
+        spyOn(locale, "getString").andReturn();
+        spyOn(alertService, "showSuccessMessage").andReturn();
+        spyOn(scope, "setCreateMode").andReturn();
+
+        // Return a mock response for createNewMobileTerminal
+        var deferred = $q.defer();
+        spyOn(mobileTerminalRestService, "createNewMobileTerminal").andReturn(deferred.promise);
+        deferred.resolve(createResponseTerminal);
+
+        // Create new terminal and check INTERNAL_ID set on scope
+        scope.createNewMobileTerminal();
+        scope.$digest();
+        expect(scope.currentMobileTerminal.mobileTerminalId.ids['INTERNAL_ID']).toBe(createResponseTerminal.mobileTerminalId.ids["INTERNAL_ID"]);
+        
+    }));    
 });
