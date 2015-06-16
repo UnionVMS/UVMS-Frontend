c4dd6dbb67c317f0a5d105c35129907ce71e6c53
emnilssol <emnilssol@mycompany.com>
2015-06-16 10:47:08 +0000

    @Review Ola, Marcus Format date/time in movementmodel.
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/unionvms-web@1992 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- app/partial/movement/manualPositionReports/manualPositionReportsModel.js
+++ app/partial/movement/manualPositionReports/manualPositionReportsModel.js
@@ -30,7 +30,7 @@ angular.module('unionvmsWeb').factory('ManualPosition', function() {
 		manualPosition.guid = data.guid;
 		manualPosition.speed = data.speed;
 		manualPosition.course = data.course;
-		manualPosition.time = data.time;
+		manualPosition.time = formatTime(data.time);
 		manualPosition.updatedTime = data.updatedTime;
 		manualPosition.status = data.status;
 		manualPosition.archived = data.archived;
@@ -81,6 +81,11 @@ angular.module('unionvmsWeb').factory('ManualPosition', function() {
 	ManualPosition.prototype.isEqualMovement = function(item) {
 		return item.id === this.id;
 	};
+
+	function formatTime (time){
+		return moment(time).format("YYYY-MM-DD hh:mm");
+	};
+
 
 	return ManualPosition;
 	
