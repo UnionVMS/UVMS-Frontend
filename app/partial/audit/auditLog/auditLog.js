cad5f9a9ac62eef0edc69ddc16f76ce3e1cc869a
emhedenma <emhedenma@mycompany.com>
2015-06-24 13:15:32 +0000

    @Review Marcus, Ola [UVMS-1140] Created audit log list.
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/unionvms-web@2301 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- app/partial/audit/auditLog/auditLog.js
+++ app/partial/audit/auditLog/auditLog.js
@@ -4,7 +4,33 @@ angular.module('unionvmsWeb').controller('AuditlogCtrl',function($scope, locale)
 
 	$scope.isAudit = true; //Highlights submenu, aka "AUDIT LOGS"
 	$scope.selectedTab = "ALL"; //Set initial tab
-	
+
+    $scope.currentSearchResults = {
+        page : 0,
+        totalNumberOfPages : 0,
+        audits : [],
+        errorMessage : "",
+        loading : false,
+        sortBy : "date",
+        sortReverse : true,
+        filter : ""
+    };
+
+    $scope.currentSearchResults.audits = [{
+        username: "nicand",
+        operation: "Add vessel create rights",
+        objectType: "User",
+        date: "2015-06-24T12:00:01+0200",
+        objectAffected: "000123"
+    },
+    {
+        username: "System",
+        operation: "Create",
+        objectType: "Automatic system report",
+        date: "2015-06-23T12:00:01+0200",
+        objectAffected: "000125"
+    }];
+
 	//Sets tabs
 	var setTabs = function (){
             return [
@@ -51,5 +77,29 @@ angular.module('unionvmsWeb').controller('AuditlogCtrl',function($scope, locale)
 		$scope.selectedTab = tab;
 	};
 
+    $scope.toggleCheckAll = function() {
+        var allChecked = $scope.isAllChecked();
+        for (var i = 0; i < $scope.currentSearchResults.audits.length; i++) {
+            $scope.currentSearchResults.audits[i].checked = !allChecked;
+        }
+    };
+
+    $scope.isAllChecked = function() {
+        if ($scope.currentSearchResults.audits.length === 0) {
+            return false;
+        }
+
+        for (var i = 0; i < $scope.currentSearchResults.audits.length; i++) {
+            if (!$scope.currentSearchResults.audits[i].checked) {
+                return false;
+            }
+        }
+
+        return true;
+    };
+
+    $scope.loadNextPage = function() {
+        // TODO
+    }
 
 });
