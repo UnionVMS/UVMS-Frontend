056c3d03fb7afcecdd64247cfdb5102b14ec7082
emnilssol <emnilssol@mycompany.com>
2015-06-25 14:02:09 +0000

    @Review Ola, Marcus UVMS-1178 Multiselect DropDown UVMS-1143 Create auditpage and searchfields inc. multiselect dropdown.
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/unionvms-web@2355 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- app/directive/common/multiselectDropDown/multiselectDropDown.js
+++ app/directive/common/multiselectDropDown/multiselectDropDown.js
@@ -1,59 +1,90 @@
-angular.module('unionvmsWeb').directive('multiselectDropDown', function() {
-	return {
-		restrict: 'E',
-		replace: true,
-		scope: {
-			model: '=',
-			options: '=',
-			pre_selected: '=preSelected',
-			checkall: '=',
-			uncheckall: '=',
-			checkallenable: '=',
-			initialtext: "@"
-		},
-		templateUrl: 'directive/common/multiselectDropDown/multiselectDropDown.html',
-		link: function(scope, element, attrs, fn) {
-			scope.selection = scope.initialtext || "Text missing";
-			console.log("model");
-			console.log(scope.model);
-			console.log("initialtext");
-			console.log(scope.initialtext);
-			
-			scope.openDropdown = function(){        
-					scope.selected_items = [];
-					for(var i=0; i<scope.pre_selected.length; i++){                        
-						scope.selected_items.push(scope.pre_selected[i].text);
-					}                                     
-			};
+angular.module('unionvmsWeb').directive('multiselectDropDown', function(locale) {
+    return {
+        restrict: 'E',
+        replace: true,
+        scope: {
+            model: '=',
+            options: '=',
+            pre_selected: '=preSelected',
+            checkall: '@',
+            uncheckall: '@',
+            initialtext: "@",
+            ngdisabled : '@'
+        },
+        templateUrl: 'directive/common/multiselectDropDown/multiselectDropDown.html',
+        link: function(scope, element, attrs, fn) {
+            scope.model = scope.model || [];
+            scope.checkAndSelectAll = false;
 
-			scope.selectAll = function () {
-				scope.model = _.pluck(scope.options, 'code');
-				console.log(scope.model);
-			};            
-			
-			scope.deselectAll = function() {
-				scope.model=[];
-				console.log(scope.model);
-			};
-			
-			scope.setSelectedItem = function(){
-				var code = this.option.code;
-				if (_.contains(scope.model, code)) {
-					scope.model = _.without(scope.model, code);
-				} else {
-					scope.model.push(code);
-				}
-				console.log(scope.model);
-				return false;
-			};
-		   
-			scope.isChecked = function (code) {                 
-				if (_.contains(scope.model, code)) {
-					return 'icon-ok pull-right';
-				}
-				return false;
-			};
-		}
-	};
+            scope.disableCheckall = false;
+            if(_.has(attrs, 'disablecheckall')){
+                scope.disableCheckall = true;
+            }
+
+            scope.ngDisabled = false;
+            if('ngdisabled' in attrs && (attrs.ngdisabled === "true" || attrs.ngdisabled === true )){
+                scope.ngDisabled = true;
+            }
+
+            scope.selection = scope.initialtext || locale.getString("common.multiple_select_dropdown_default_text");
+            scope.checkAll = scope.checkall;
+            scope.unCheckAll = scope.uncheckall;
+
+            scope.openDropdown = function(){
+                for(var i=0; i<scope.pre_selected.length; i++){                        
+                    scope.selectedItems.push(scope.pre_selected[i].text);
+                }                                     
+            };
+
+            scope.selectAll = function () {
+                scope.model = _.pluck(scope.options, 'code');
+                scope.setTitle();
+                scope.checkAndSelectAll = true; 
+            };            
+            
+            scope.deselectAll = function() {
+                scope.model=[];
+                scope.setTitle();
+                scope.checkAndSelectAll = false; 
+            };
+            
+            scope.setSelectedItem = function(){
+                var code = this.option.code;
+                if (_.contains(scope.model, code)) {
+                    scope.model = _.without(scope.model, code);
+                    scope.checkAndSelectAll = false; 
+                } else {
+                    scope.model.push(code);
+                    if(scope.model.length === scope.options.length)
+                    {
+                        scope.checkAndSelectAll = true;
+                    }
+                }
+                scope.setTitle();
+                return false;
+            };
+           
+            scope.isChecked = function (code) {                 
+                if (_.contains(scope.model, code)) {
+                    return true;
+                }
+                return false;
+            };
+
+            scope.setTitle = function(){
+                switch(scope.model.length) {
+                    case 0:
+                        scope.selection = scope.initialtext || locale.getString("common.multiple_select_dropdown_default_text");
+                        break;
+                    case 1:
+                        scope.selection = locale.getString('common.one_selected');
+                        break;
+                    default:
+                        scope.selection = locale.getString('common.multiple_selected');
+                        break;
+                }                   
+            };
+        }
+    };
 });
 
--- app/directive/common/search/advancedSearch/audit/auditSearchForm.js
+++ app/directive/common/search/advancedSearch/audit/auditSearchForm.js
@@ -2,16 +2,22 @@ angular.module('unionvmsWeb').controller('AuditsearchformCtrl',function($scope, 
 	
 	$scope.objectTypes = []; //Objecttype dropdown
 	$scope.objectTypes.push({"text":"Assets", "code":"Assets"});
-    $scope.objectTypes.push({"text":"Reports", "code":"Reports"});
-    $scope.objectTypes.push({"text":"Mobile terminal", "code":"Mobile terminal"});
-    $scope.objectTypes.push({"text":"Zone system", "code":"Zone system"});
+	$scope.objectTypes.push({"text":"Reports", "code":"Reports"});
+	$scope.objectTypes.push({"text":"Mobile terminal", "code":"Mobile terminal"});
+	$scope.objectTypes.push({"text":"Zone system", "code":"Zone system"});
 
 
 	$scope.operations = []; //Operation dropdown
-    $scope.operations.push({"text":"Create", "code":"Create"});
-    $scope.operations.push({"text":"Update", "code":"Update"});
-    $scope.operations.push({"text":"Remove", "code":"Remove"});
-    
-    $scope.selectedItems = [];
+	$scope.operations.push({"text":"Create", "code":"Create"});
+	$scope.operations.push({"text":"Update", "code":"Update"});
+	$scope.operations.push({"text":"Remove", "code":"Remove"});
+	
+	$scope.selectedItems = [];
+	$scope.selectedObjectTypes = [];
+
+	$scope.member = {
+		operations:[],
+		objecttypes:[]
+	};
 
 });
