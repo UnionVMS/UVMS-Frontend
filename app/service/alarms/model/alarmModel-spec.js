2a462b0a32dba269641507489ff9105420959128
emperssgu <emperssgu@mycompany.com>
2015-10-13 07:45:06 +0000

    @Review Marcus [UVMS-1805] Updated alarm model.
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/unionvms-web@5450 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- app/service/alarms/model/alarmModel-spec.js
+++ app/service/alarms/model/alarmModel-spec.js
@@ -2,11 +2,14 @@ describe('Alarm', function() {
 
     var alarmData = {
         guid: "abcdd-12345-fewf",
-        affectedObject: "dummy_guid",
+        assetId: {
+            type: "DUMMY_ASSET_TYPE_1",
+            value: "DUMMY_ASSET_TYPE_VALUE_1"
+        },
         openDate: "2015-10-09 08:13:09 +0200",
         resolveDate: "2015-10-09 08:13:09 +0200",
         resolvedBy: "close user",
-        ruleTriggered: "Da Rule",
+        ruleName: "Da Rule",
         sender: "SWE",
         status: "CLOSED",
     };
@@ -19,8 +22,9 @@ describe('Alarm', function() {
 
         expect(alarm.guid).toEqual(alarmData.guid);
         expect(alarm.openedDate).toEqual(alarmData.openDate);
-        expect(alarm.affectedObject).toEqual(alarmData.affectedObject);
-        expect(alarm.ruleName).toEqual(alarmData.ruleTriggered);
+        expect(alarm.assetId.type).toEqual(alarmData.assetId.type);
+        expect(alarm.assetId.value).toEqual(alarmData.assetId.value);
+        expect(alarm.ruleName).toEqual(alarmData.ruleName);
         expect(alarm.sender).toEqual(alarmData.sender);
         expect(alarm.resolvedDate).toEqual(alarmData.resolveDate);
         expect(alarm.resolvedBy).toEqual(alarmData.resolvedBy);
@@ -43,5 +47,54 @@ describe('Alarm', function() {
         alarm.status = "";
         expect(alarm.isOpen()).toBeFalsy();
     }));
+
+    it("isClosed should return true only when status is CLOSED", inject(function(Alarm) {
+        var alarm = Alarm.fromDTO(alarmData);
+        alarm.status = "CLOSED";
+        expect(alarm.isClosed()).toBeTruthy();
+
+        alarm.status = "closed";
+        expect(alarm.isClosed()).toBeTruthy();
+
+        alarm.status = "OPEN";
+        expect(alarm.isClosed()).toBeFalsy();
+
+        alarm.status = "";
+        expect(alarm.isClosed()).toBeFalsy();
+    }));
+
+    it("isPending should return true only when status is PENDING", inject(function(Alarm) {
+        var alarm = Alarm.fromDTO(alarmData);
+        alarm.status = "PENDING";
+        expect(alarm.isPending()).toBeTruthy();
+
+        alarm.status = "pending";
+        expect(alarm.isPending()).toBeTruthy();
+
+        alarm.status = "OPEN";
+        expect(alarm.isPending()).toBeFalsy();
+
+        alarm.status = "";
+        expect(alarm.isPending()).toBeFalsy();
+    }));
+
+    it("isVesselAsset() should return true only when assetId type is VESSEL", inject(function(Alarm) {
+        var alarm = new Alarm();
+        expect(alarm.isVesselAsset().toBeFalsy();
+
+        alarm.assetId = {
+            type : "ASSET",
+            value : "test-123"
+        };
+        expect(alarm.isVesselAsset().toBeTruthy();
+
+       alarm.assetId = {
+            type : "OTHER_TYPE",
+            value : "test-123"
+        };
+
+        expect(alarm.isVesselAsset().toBeFalsy();
+    }));
+
 
 });
--- app/service/alarms/model/alarmModel.js
+++ app/service/alarms/model/alarmModel.js
@@ -3,7 +3,8 @@ angular.module('unionvmsWeb').factory('Alarm', function() {
     function Alarm(){
         this.guid = undefined;
         this.openedDate = undefined;
-        this.affectedObject = undefined;
+        this.assetId = undefined;
+        this.vessel = undefined;
         this.ruleName = undefined;
         this.sender = undefined;
         this.resolvedDate = undefined;
@@ -15,8 +16,15 @@ angular.module('unionvmsWeb').factory('Alarm', function() {
         var alarm = new Alarm();
         alarm.guid = dto.guid;
         alarm.openedDate = dto.openDate;
-        alarm.affectedObject = dto.affectedObject;
-        alarm.ruleName = dto.ruleTriggered;
+
+        if(angular.isDefined(dto.assetId)){
+            alarm.assetId = {
+                type : dto.assetId.type,
+                value : dto.assetId.value
+            };
+        }
+
+        alarm.ruleName = dto.ruleName;
         alarm.sender = dto.sender;
         alarm.resolvedDate = dto.resolveDate;
         alarm.resolvedBy = dto.resolvedBy;
@@ -36,16 +44,34 @@ angular.module('unionvmsWeb').factory('Alarm', function() {
         return this.status.toUpperCase() === "OPEN";
     };
 
+    Alarm.prototype.isPending = function() {
+        return this.status.toUpperCase() === "PENDING";
+    };
+
     Alarm.prototype.isClosed = function() {
         return this.status.toUpperCase() === "CLOSED";
     };
+
+
+    Alarm.prototype.isVesselAsset = function() {
+        if(angular.isDefined(this.assetId) && angular.isDefined(this.assetId.type)){
+            return this.assetId.type.toUpperCase() === 'VESSEL';
+        };
+        return false;
+    };
+
 
     Alarm.prototype.copy = function() {
         var copy = new Alarm();
 
         copy.guid = this.guid;
         copy.openedDate = this.openedDate;
-        copy.affectedObject = this.affectedObject;
+        if(this.assetId){
+            copy.assetId = {
+                type : this.assetId.type,
+                value : this.assetId.value
+            };
+        }
         copy.ruleName = this.ruleName;
         copy.sender = this.sender;
         copy.resolvedDate = this.resolvedDate;
