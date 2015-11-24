48fa350c537895d6394fe49ff86cab3a0a96a5f3
emmatosjo <emmatosjo@mycompany.com>
2015-11-24 10:47:49 +0000

    SC - removed userPreferencesPanel(not used)
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/unionvms-web@6949 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- app/partial/spatial/configurations/userPreferencesPanel/userPreferencesCollapsiblePanel.js
+++
@@ -1,7 +0,0 @@
-angular.module('unionvmsWeb').controller('UserpreferencescollapsiblepanelCtrl',function($scope){
-
-	$scope.status = {
-		userPreferencesOpen: false
-	};
-	
-});
--- app/partial/spatial/configurations/userPreferencesPanel/userPreferencesPanelForm/userPreferencesPanelForm.js
+++
@@ -1,4 +0,0 @@
-angular.module('unionvmsWeb').controller('UserpreferencespanelformCtrl',function($scope){
-
-
-});
