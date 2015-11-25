6ec0e588e98f889c308b87ed44d2ad5a2693783e
emperssgu <emperssgu@mycompany.com>
2015-11-25 13:29:06 +0000

    @Review Marcus [UVMS-2263] Updated Vessel CFR validation rule.
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/unionvms-web@7004 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- app/service/vessel/vesselValidationService.js
+++ app/service/vessel/vesselValidationService.js
@@ -2,8 +2,8 @@ angular.module('unionvmsWeb').factory('vesselValidationService',function() {
 
 	var vesselValidationService = {
         getCFRPattern : function(){
-            //3 letters followed by 9 digits
-            return new RegExp(/^[a-zA-Z]{3}[0-9]{9}$/);
+            //3 letters followed by 9 digits an/or letters (including swedish åäö)
+            return new RegExp(/^[a-zA-ZåäöÅÄÖ]{3}[0-9a-zA-ZåäöÅÄÖ]{9}$/);
         },
         getMMSIPattern : function(){
             //9 digits
