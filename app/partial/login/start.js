8903ea6da67db1732940603871a8d9c0148b7648
emperssgu <emperssgu@mycompany.com>
2015-11-26 10:23:24 +0000

    @Review Marcus [UVMS-2262] Bug fix for setting home page to a page the user has access to.
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/unionvms-web@7022 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- app/partial/login/start.js
+++ app/partial/login/start.js
@@ -12,55 +12,78 @@ angular.module('unionvmsWeb').factory('startPageService',function($log, globalSe
         return userService.isAllowed(feature,module,true);
     };
 
+    //Matches pages (in global config) with states in the app
+    //Order of pages and states are of importance only when user don't have access to the homepage set in the global config
+    var pagesAndStates = {
+        exchange : ['app.exchange'],
+        positions : ['app.movement'],
+        polling : ['app.pollingLogs'],
+        mobileTerminals : ['app.communication'],
+        assets : ['app.assets'],
+        alarms : ['app.holdingTable', 'app.openTickets'],
+        admin : ['app.auditLog'],
+        user : ['app.usm.users'],
+    };
+
+    var userHasAccessToState = function(state){
+        var accessToAssetsAndTerminals = checkAccess(unionVMSApplication, 'viewVesselsAndMobileTerminals');
+
+        switch(state){
+            case 'app.movement':
+                return checkAccess('Movement', 'viewMovements') && accessToAssetsAndTerminals;
+            case 'app.exchange':
+                return checkAccess('Exchange', 'viewExchange');
+            case 'app.pollingLogs':
+                return checkAccess(unionVMSApplication, 'viewMobileTerminalPolls') && accessToAssetsAndTerminals;
+            case 'app.communication':
+                return accessToAssetsAndTerminals;
+            case 'app.assets':
+                return accessToAssetsAndTerminals;
+            case 'app.holdingTable':
+                return checkAccess(unionVMSApplication, 'viewAlarmsHoldingTable');
+            case 'app.openTickets':
+                return checkAccess(unionVMSApplication, 'viewAlarmsOpenTickets');
+            case 'app.auditLog':
+                return checkAccess('Audit', 'viewAudit');
+            case 'app.usm.users':
+                return checkAccess('USM', 'viewUsers');
+            default:
+                $log.info("State: " +state +" is missing from list. Returning false.");
+                return false;
+        }
+    };
+
     var startPageService = {
         getStartPageStateName : function(){
             var defaultHomePage = globalSettingsService.get('defaultHomePage', false);
-            var homeState = 'app.exchange';
-            switch(defaultHomePage){
-                case 'positions':
-                    if(checkAccess('Movement', 'viewMovements')){
-                        homeState = 'app.movement';
+            var possibleStates, i;
+
+            //Check if user has access to the defaultHomePage
+            if(defaultHomePage in pagesAndStates){
+                possibleStates = pagesAndStates[defaultHomePage];
+                for (i=0; i< possibleStates.length; i++){
+                    if(userHasAccessToState(possibleStates[i])){
+                        return possibleStates[i];
                     }
-                    break;
-                case 'exchange':
-                    if(checkAccess('Exchange', 'viewExchange')){
-                        homeState = 'app.exchange';
-                    }
-                    break;
-                case 'polling':
-                    if(checkAccess(unionVMSApplication, 'viewMobileTerminalPolls')){
-                        homeState = 'app.pollingLogs';
-                    }
-                    break;
-                case 'mobileTerminals':
-                    if(checkAccess(unionVMSApplication, 'viewVesselsAndMobileTerminals')){
-                        homeState = 'app.communication';
-                    }
-                    break;
-                case 'assets':
-                    if(checkAccess(unionVMSApplication, 'viewVesselsAndMobileTerminals')){
-                        homeState = 'app.assets';
-                    }
-                    break;
-                case 'alarms':
-                    if(checkAccess(unionVMSApplication, 'viewAlarmsHoldingTable')){
-                        homeState = 'app.holdingTable';
-                    }
-                    else if(checkAccess(unionVMSApplication, 'viewAlarmsOpenTickets')){
-                        homeState = 'app.openTickets';
-                    }
-                    break;
-                case 'admin':
-                    if(checkAccess('Audit', 'viewAudit')){
-                        homeState = 'app.auditLog';
-                    }
-                    break;
-                default:
-                    $log.info("Default home page is set to: " +defaultHomePage +" but not state is defined for that page. Redirecting to exchange.");
-                    homeState = 'app.exchange';
-                    break;
+                }
             }
-            return homeState;
+
+            //User didn't have access to the default home page, so lets find first page user has access to
+            $log.info("Default home page is set to '" +defaultHomePage +"' but user don't have access to this page. Redirecting to first page user has access to.");
+            for (var page in pagesAndStates) {
+                if (pagesAndStates.hasOwnProperty(page)) {
+                    possibleStates = pagesAndStates[page];
+                    for (i = 0; i< possibleStates.length; i++){
+                        if(userHasAccessToState(possibleStates[i])){
+                            return possibleStates[i];
+                        }
+                    }
+                }
+            }
+
+            //No access, just return exchange page
+            $log.warn("User don't have access to any page. Return access error.");
+            return 'uvmsAccessError';
         }
     };
 
