64b36cd2c0e751b27169c3f027284731a0b6454a
emnilssol <emnilssol@mycompany.com>
2015-11-27 13:20:26 +0000

    Review Ola Fixed test for dataNotAvailable-spec.js
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/unionvms-web@7072 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- app/filter/common/dataNotAvailable/dataNotAvailable-spec.js
+++ app/filter/common/dataNotAvailable/dataNotAvailable-spec.js
@@ -1,13 +1,29 @@
 describe('dataNotAvailable', function() {
 
+	var dataNotAvailable;
+	var locale;
+
 	beforeEach(module('unionvmsWeb'));
+    beforeEach(function() {
+        locale = {
+            getString : function(str){
+                if(str === 'common.short_not_available'){}
+                    return 'N/A';
+            }
+        }
+        module(function ($provide) {
+          $provide.value('locale', locale);
+        });
+    });
 
-	it('should ...', inject(function($filter) {
+	it('should return N/A when undefined', inject(function($filter){
+		var filter = $filter('dataNotAvailable');
+		expect(filter(undefined)).toBe('N/A');
+	}));
 
-        var filter = $filter('dataNotAvailable');
-
-		expect(filter('input')).toEqual('output');
-
+	it('should return input as output ', inject(function($filter){
+		var filter = $filter('dataNotAvailable');
+		expect(filter('DATA')).toBe('DATA');
 	}));
 
 });
