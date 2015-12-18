ce6d0370aa0a4325cb1a32618cd01ffe305473fc
emperssgu <emperssgu@mycompany.com>
2015-12-18 12:45:34 +0000

    @Review Ruxandra Changed preload icon.
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/unionvms-web@7511 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- app/directive/common/loadingIndicator/loadingIndicator.js
+++ app/directive/common/loadingIndicator/loadingIndicator.js
@@ -15,8 +15,12 @@ angular.module('unionvmsWeb').directive('loadingIndicator', function($compile) {
                 spinnerHTML = '<i class="fa fa-spinner fa-spin fa-pulse"></i>';
             }
             //CIRCLE BOUNCE
-            else{
+            else if(scope.type === "BOUNCE"){
                 spinnerHTML = '<div class="circle"></div><div class="circle"></div>';
+            }
+            //SHIP
+            else{
+                spinnerHTML = '<div class="spinningShipContainer"><div></div></div>';
             }
 
             elem.append($compile('<div ng-show="loadingIndicator" class="loadingIndicator" ng-class="size"><div class="spinnerOverlay"></div><div class="spinner">' +spinnerHTML +'</div><div class="loadingText" ng-show="message"><span ng-bind="message"></span></div></div>')(scope));
