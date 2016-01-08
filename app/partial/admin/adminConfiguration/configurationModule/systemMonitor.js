b90153ee81b83f9feb3b23d4f4181ec17e6e97d0
emhedenma <emhedenma@mycompany.com>
2016-01-08 12:40:50 +0000

    Added loading indicator to system monitor.
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/unionvms-web@7591 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- app/partial/admin/adminConfiguration/configurationModule/systemMonitor.js
+++ app/partial/admin/adminConfiguration/configurationModule/systemMonitor.js
@@ -1,7 +1,9 @@
 angular.module('unionvmsWeb').controller('SystemMonitorController', function($scope, $resource) {
 
+	$scope.loading = true;
 	$resource("/config/rest/pings").get(function(response) {
 		$scope.pings = response.data;
+		$scope.loading = false;
 	});
 
 	$scope.statusLabel = function(status) {
