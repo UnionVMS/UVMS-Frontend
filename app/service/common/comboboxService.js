82e9ff76351112a646f2e27a2d3fd4d716850558
emmatosjo <emmatosjo@mycompany.com>
2016-02-01 13:54:23 +0000

    SC[UNIONVMS] - fixed combobox when has scrollbar
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/unionvms-web@8771 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- app/service/common/comboboxService.js
+++ app/service/common/comboboxService.js
@@ -12,14 +12,17 @@ angular.module('unionvmsWeb').factory('comboboxService', function($window) {
 
 	var positionComboList = function() {
     	if(activeCombo && activeCombo.isOpen){
-            var relativePos = 0;
-            var buttonPosition = $(activeCombo.element).offset();
-    		buttonPosition.top += $(activeCombo.element).height();
+            var relativePos = {'top':0,'left':0};
             if(activeCombo.destComboList){
-                relativePos = $(activeCombo.destComboList).offset().top;
+                relativePos = $(activeCombo.destComboList).offset();
             }
+            var comboButtonHeight = $(activeCombo.element).height();
+            var buttonPosition = $(activeCombo.element).offset();
+    		buttonPosition.top += comboButtonHeight - relativePos.top;
+    		buttonPosition.left -= relativePos.left;
+            
     		activeCombo.comboContainer = $('#' + activeCombo.comboboxId);
-    		$(activeCombo.comboContainer).css('top', buttonPosition.top - relativePos);
+    		$(activeCombo.comboContainer).css('top', buttonPosition.top);
     		$(activeCombo.comboContainer).css('left', buttonPosition.left);
 		
 			$(activeCombo.comboContainer).width($(activeCombo.element).width());
@@ -27,17 +30,23 @@ angular.module('unionvmsWeb').factory('comboboxService', function($window) {
     		var comboMenu = $('#' + activeCombo.comboboxId + '>.dropdown-menu');
     		var footerTop = $(window).height() - angular.element('footer')[0].offsetHeight;
     		var bottomSpace = footerTop - buttonPosition.top;
-
     		var topSpace = $(activeCombo.element).offset().top;
-    		
-    		if($(activeCombo.element).height() > bottomSpace){
+    		if($('.fullscreen').length > 0){
+    			bottomSpace = $(activeCombo.destComboList).height() - buttonPosition.top;
+    			topSpace -= relativePos.top;
+    		}
+
+    		if((activeCombo.loadedItems.length * 26 > 300 ? 300 : activeCombo.loadedItems.length * 26) > bottomSpace){
     			if(topSpace > bottomSpace){
-    				activeCombo.comboContainer.css('top', topSpace - relativePos);
-    				if(topSpace < $(activeCombo.element).height()){
-    					activeCombo.comboContainer.css('height', topSpace);
+    				var comboHeight = activeCombo.loadedItems.length * 26 + 16;
+    				comboHeight += (activeCombo.initialValue && activeCombo.initialValue.text ? 26 : 0);
+    				comboHeight = (comboHeight > 300 ? 300 + 4 : comboHeight);
+					activeCombo.comboContainer.css('top', buttonPosition.top - comboButtonHeight - comboHeight);
+    				if(topSpace < activeCombo.loadedItems.length * 26){
+    					activeCombo.comboContainer.css('max-height', topSpace);
         			}
     			}else{
-    				activeCombo.comboContainer.css('height', topSpace);
+    				activeCombo.comboContainer.css('max-height', topSpace);
     			}
     		}
     	}
