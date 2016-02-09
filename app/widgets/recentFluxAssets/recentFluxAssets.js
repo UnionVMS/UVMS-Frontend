dbd5f879d2c8d2fbebeac43fffbc6e511a2f0f17
emhedenma <emhedenma@mycompany.com>
2016-02-09 09:06:36 +0000

    UVMS-2586: Created widget for showing assets with FLUX activity in last 24h.
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/unionvms-web@8963 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

---
+++ app/widgets/recentFluxAssets/recentFluxAssets.js
@@ -0,0 +1,94 @@
+(function() {
+
+	var app = angular.module('activeFluxAssets', ['ngResource']);
+	app.directive('activeFluxAssets', ActiveFluxAssetsDirective);
+	app.controller('activeFluxAssetsController', ActiveFluxAssetsController);
+	app.provider('activeFluxAssetsService', ActiveFluxAssetsServiceProvider);
+
+	ActiveFluxAssetsController.$inject = ['activeFluxAssetsService'];
+
+	function ActiveFluxAssetsDirective(){
+		return {
+			controller: 'activeFluxAssetsController as ctrl',
+			restrict: 'E',
+			templateUrl: 'widgets/recentFluxAssets/recentFluxAssets.html', // temporary location
+		};
+	};
+
+	function ActiveFluxAssetsController(activeFluxAssetsService) {
+		this.items = activeFluxAssetsService.getAssets();
+
+		var i18n = {
+			nations: {
+				at: "Austria",
+				be: "Belgium",
+				bg: "Bulgaria",
+				cy: "Cyprus",
+				cz: "Czech Republic",
+				de: "Germany",
+				dk: "Denmark",
+				ee: "Estonia",
+				es: "Spain",
+				fi: "Finland",
+				fr: "France",
+				gb: "Great Britain",
+				gr: "Greece",
+				hr: "Croatia",
+				hu: "Hungary",
+				ie: "Ireland",
+				im: "Isle of Man",
+				it: "Italy",
+				lt: "Lithuania",
+				lu: "Luxembourg",
+				lv: "Latvia",
+				mt: "Malta",
+				nl: "Netherlands",
+				pl: "Poland",
+				pt: "Portugal",
+				ro: "Romania",
+				si: "Slovenia",
+				sk: "Slovakia",
+				sv: "Sweden"
+			},
+			"header": "Active FLUX assets",
+			"subheader": "With positions received in the last 24h."
+		};
+
+		this.i18n = function(key) {
+			return i18n[key];
+		};
+
+		this.nationName = function(nation) {
+			return i18n.nations[nation] || nation;
+		};
+	}
+
+	function ActiveFluxAssetsServiceProvider() {
+		var remoteBaseUrl = undefined;
+		var useDummyData = false;
+		this.setRemoteBaseUrl = function(url) {
+			remoteBaseUrl = url;
+		};
+		this.setUseDummyData = function(bool) {
+			useDummyData = bool;
+		};
+		this.$get = ['$resource', 'recentFluxAssetsDummyData', function($resource, dummyData) {
+			return new ActiveFluxAssetsService($resource, remoteBaseUrl, useDummyData ? dummyData : undefined);
+		}];
+	}
+
+	function ActiveFluxAssetsService($resource, remoteBaseUrl, dummyData) {
+		return {
+			getAssets: getAssets
+		};
+
+		function getAssets() {
+			if (dummyData) {
+				return dummyData;
+			}
+
+			return $resource(remoteBaseUrl + "/assets").query();
+		}
+	}
+
+})();
---
+++ app/widgets/recentFluxAssets/recentFluxAssetsDummyData.js
@@ -0,0 +1,121 @@
+(function() {
+	var app = angular.module('activeFluxAssets');
+	app.constant('recentFluxAssetsDummyData', [{
+		"nation":"at",
+		"count": 3
+	},
+	{
+		"nation":"be",
+		"count": 5
+	},
+	{
+		"nation":"bg",
+		"count": 1
+	},
+	{
+		"nation":"cy",
+		"count": 12
+	},
+	{
+		"nation":"cz",
+		"count": 18
+	},
+	{
+		"nation":"de",
+		"count": 56
+	},
+	{
+		"nation":"dk",
+		"count": 112
+	},
+	{
+		"nation":"ee",
+		"count": 19
+	},
+	{
+		"nation":"es",
+		"count": 0
+	},
+	{
+		"nation":"fi",
+		"count": 0
+	},
+	{
+		"nation":"fr",
+		"count": 1
+	},
+	{
+		"nation":"gb",
+		"count": 7
+	},
+	{
+		"nation":"gr",
+		"count": 17
+	},
+	{
+		"nation":"hr",
+		"count": 9
+	},
+	{
+		"nation":"hu",
+		"count": 3
+	},
+	{
+		"nation":"ie",
+		"count": 12
+	},
+	{
+		"nation":"im",
+		"count": 9
+	},
+	{
+		"nation":"it",
+		"count": 91
+	},
+	{
+		"nation":"lt",
+		"count": 100
+	},
+	{
+		"nation":"lu",
+		"count": 1
+	},
+	{
+		"nation":"lv",
+		"count": 0
+	},
+	{
+		"nation":"mt",
+		"count": 2
+	},
+	{
+		"nation":"nl",
+		"count": 11
+	},
+	{
+		"nation":"pl",
+		"count": 17
+	},
+	{
+		"nation":"pt",
+		"count": 67
+	},
+	{
+		"nation":"ro",
+		"count": 23
+	},
+	{
+		"nation":"si",
+		"count": 0
+	},
+	{
+		"nation":"sk",
+		"count": 2
+	},
+	{
+		"nation":"sv",
+		"count": 2
+	}]);
+})();
+
+
