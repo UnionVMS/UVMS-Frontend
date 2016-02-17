7c7b0a62ae8bd7aa17a8d9f51cedff5fac7af1e6
emhedenma <emhedenma@mycompany.com>
2016-02-17 08:59:55 +0000

    Brought UVMS into 2016.
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/unionvms-web@9100 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- app/partial/footer/footer.js
+++ app/partial/footer/footer.js
@@ -1,4 +1,5 @@
 angular.module('unionvmsWeb').controller('FooterCtrl',function($scope, envConfig){
     $scope.envName = envConfig.env_name;
     $scope.apiURL = envConfig.rest_api_base;
-});  
+    $scope.fullYear = new Date().getFullYear();
+});
