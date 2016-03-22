d6148ee919812b38c6365d3d99fdc3a9098d7fa3
emhedenma <emhedenma@mycompany.com>
2016-03-22 08:52:37 +0000

    Wrapped code in IIFE.
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/unionvms-web@9989 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- app/partial/common/error/error.js
+++ app/partial/common/error/error.js
@@ -1,36 +1,40 @@
-var app = angular.module('unionvmsWeb');
+(function() {
+    'use strict';
 
-app.controller('errorCtrl', function($scope, errorService, locale) {
-    //Set texts
-    $scope.error = errorService.getErrorMessage();
-    //set the header text
-    $scope.title = locale.getString('common.loading_page_error');
-    if($scope.title.indexOf('%%KEY_NOT_FOUND%%') >= 0){
-        $scope.title = 'Error loading page';
-    }
-    //set the error text
-    $scope.errorText = locale.getString('common.loading_page_error_text');
-    if($scope.errorText.indexOf('%%KEY_NOT_FOUND%%') >= 0){
-        $scope.errorText = '';
-    }
-    //set the error details header text
-    $scope.errorDetailsTitle = locale.getString('common.loading_page_error_details');
-    if($scope.errorDetailsTitle.indexOf('%%KEY_NOT_FOUND%%') >= 0){
-        $scope.errorDetailsTitle = 'Error details:';
-    }
+    var app = angular.module('unionvmsWeb');
 
-});
+    app.controller('errorCtrl', function($scope, errorService, locale) {
+        //Set texts
+        $scope.error = errorService.getErrorMessage();
+        //set the header text
+        $scope.title = locale.getString('common.loading_page_error');
+        if($scope.title.indexOf('%%KEY_NOT_FOUND%%') >= 0){
+            $scope.title = 'Error loading page';
+        }
+        //set the error text
+        $scope.errorText = locale.getString('common.loading_page_error_text');
+        if($scope.errorText.indexOf('%%KEY_NOT_FOUND%%') >= 0){
+            $scope.errorText = '';
+        }
+        //set the error details header text
+        $scope.errorDetailsTitle = locale.getString('common.loading_page_error_details');
+        if($scope.errorDetailsTitle.indexOf('%%KEY_NOT_FOUND%%') >= 0){
+            $scope.errorDetailsTitle = 'Error details:';
+        }
 
-angular.module('unionvmsWeb')
-    .factory('errorService',function() {
-
-        var errorMessage;
-        return {
-            getErrorMessage : function(){
-                return errorMessage;
-            },
-            setErrorMessage : function(message){
-                errorMessage = message;
-            },
-        };
     });
+
+    angular.module('unionvmsWeb')
+        .factory('errorService',function() {
+
+            var errorMessage;
+            return {
+                getErrorMessage : function(){
+                    return errorMessage;
+                },
+                setErrorMessage : function(message){
+                    errorMessage = message;
+                },
+            };
+        });
+})();
