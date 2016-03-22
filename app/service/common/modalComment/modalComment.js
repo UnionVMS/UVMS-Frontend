f276bdc9a6e1b142ccb5332fc4ea90d1b052ee95
emhedenma <emhedenma@mycompany.com>
2016-03-22 09:02:00 +0000

    Wrapped code in IIFE.
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/unionvms-web@9993 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- app/service/common/modalComment/modalComment.js
+++ app/service/common/modalComment/modalComment.js
@@ -1,40 +1,44 @@
-var app = angular.module('unionvmsWeb');
+(function() {
+    'use strict';
 
-app.controller('modalCommentCtrl', function($scope, $modalInstance, options, locale) {
-    $scope.submitAttempted = false;
-	$scope.comment = "";
-	$scope.labels = {
-		title: options.titleLabel || locale.getString("common.comment"),
-		save: options.saveLabel || locale.getString("common.save"),
-		cancel: options.cancelLabel || locale.getString("common.cancel"),
-		placeholder: options.placeholderLabel || locale.getString("common.comment_modal_comment_placeholder")
-	};
+    var app = angular.module('unionvmsWeb');
 
-	$scope.save = function() {
-        $scope.submitAttempted = true;
-        if($scope.commentForm.$valid) {
-            $modalInstance.close($scope.comment);
-        }
-	};
+    app.controller('modalCommentCtrl', function($scope, $modalInstance, options, locale) {
+        $scope.submitAttempted = false;
+        $scope.comment = "";
+        $scope.labels = {
+            title: options.titleLabel || locale.getString("common.comment"),
+            save: options.saveLabel || locale.getString("common.save"),
+            cancel: options.cancelLabel || locale.getString("common.cancel"),
+            placeholder: options.placeholderLabel || locale.getString("common.comment_modal_comment_placeholder")
+        };
 
-	$scope.cancel = function() {
-		$modalInstance.dismiss();
-	};
-});
+        $scope.save = function() {
+            $scope.submitAttempted = true;
+            if($scope.commentForm.$valid) {
+                $modalInstance.close($scope.comment);
+            }
+        };
 
-app.factory('modalComment', function($modal) {
-	return {
-		open: function(callback, options) {
-			$modal.open({
-				templateUrl: "service/common/modalComment/modalComment.html",
-				controller: "modalCommentCtrl",
-                backdrop: 'static', //will not close when clicking outside the modal window
-				resolve: {
-					options: function() {
-						return options || {};
-					}
-				}
-			}).result.then(callback);
-		}
-	};
-});
+        $scope.cancel = function() {
+            $modalInstance.dismiss();
+        };
+    });
+
+    app.factory('modalComment', function($modal) {
+        return {
+            open: function(callback, options) {
+                $modal.open({
+                    templateUrl: "service/common/modalComment/modalComment.html",
+                    controller: "modalCommentCtrl",
+                    backdrop: 'static', //will not close when clicking outside the modal window
+                    resolve: {
+                        options: function() {
+                            return options || {};
+                        }
+                    }
+                }).result.then(callback);
+            }
+        };
+    });
+})();
