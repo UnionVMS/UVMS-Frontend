90f017ffe3908b4efb8097d467ca9ae6d659d716
emhedenma <emhedenma@mycompany.com>
2016-03-22 08:55:06 +0000

    Wrapped code in IIFE.
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/unionvms-web@9991 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- app/partial/help/help.js
+++ app/partial/help/help.js
@@ -1,123 +1,125 @@
-var app = angular.module('unionvmsWeb');
+(function() {
+	var app = angular.module('unionvmsWeb');
 
-app.controller('help.controller', HelpController);
+	app.controller('help.controller', HelpController);
 
-HelpController.$inject = ['help.lang', 'help.toc', '$anchorScroll', '$location'];
+	HelpController.$inject = ['help.lang', 'help.toc', '$anchorScroll', '$location'];
 
-function HelpController(lang, toc, $anchorScroll, $location) {
-	toc.reset();
-	this.sections = toc.sections;
-	this.titles = toc.titles;
-	this.goTo = goTo;
-	this.localizedUrl = 'partial/help/help-' + lang + '.html';
-	this.localizedSrc = localizedUrl;
-	this.localize = function(url) {
-		return 'partial/help/' + lang + '/' + url;
-	};
+	function HelpController(lang, toc, $anchorScroll, $location) {
+		toc.reset();
+		this.sections = toc.sections;
+		this.titles = toc.titles;
+		this.goTo = goTo;
+		this.localizedUrl = 'partial/help/help-' + lang + '.html';
+		this.localizedSrc = localizedUrl;
+		this.localize = function(url) {
+			return 'partial/help/' + lang + '/' + url;
+		};
 
-	this.pages = [
-		'00-welcome.html',
-		'01-login.html',
-		'02-overview.html',
-		'03-assets.html',
-		'04-mobileTerminals.html',
-		'05-positions.html',
-		'06-polling.html',
-		'07-alarms.html',
-		'08-exchange.html',
-		'09-reports.html',
-		'10-user.html',
-		'11-glossary.html'
-	];
+		this.pages = [
+			'00-welcome.html',
+			'01-login.html',
+			'02-overview.html',
+			'03-assets.html',
+			'04-mobileTerminals.html',
+			'05-positions.html',
+			'06-polling.html',
+			'07-alarms.html',
+			'08-exchange.html',
+			'09-reports.html',
+			'10-user.html',
+			'11-glossary.html'
+		];
 
-	function goTo(index) {
-		$location.hash(index);
-		$anchorScroll(index);
+		function goTo(index) {
+			$location.hash(index);
+			$anchorScroll(index);
+		}
+
+		function localizedUrl(url) {
+			return 'assets/help/img/' + lang + '/' + url;
+		}
 	}
 
-	function localizedUrl(url) {
-		return 'assets/help/img/' + lang + '/' + url;
-	}
-}
+	app.constant('help.langReplacements', {
+		"sv": "en"
+	});
 
-app.constant('help.langReplacements', {
-	"sv": "en"
-});
+	app.factory('help.lang', ['locale', 'help.langReplacements', function(locale, replacements) {
+		var lang = locale.getLocale();
+		var index = lang.indexOf('-');
+		if (index >= 0) {
+			// example: 'en-us' -> 'en'
+			lang = lang.substring(0, index);
+		}
 
-app.factory('help.lang', ['locale', 'help.langReplacements', function(locale, replacements) {
-	var lang = locale.getLocale();
-	var index = lang.indexOf('-');
-	if (index >= 0) {
-		// example: 'en-us' -> 'en'
-		lang = lang.substring(0, index);
-	}
+		return replacements[lang] || lang;
+	}]);
 
-	return replacements[lang] || lang;
-}]);
+	app.factory('help.toc', [function() {
 
-app.factory('help.toc', [function() {
+		var toc = {};
+		toc.sections = [];
+		toc.titles = {};
 
-	var toc = {};
-	toc.sections = [];
-	toc.titles = {};
+		var sectionIndexes = {};
+		function getInsertionIndex(sectionIndex) {
+			var i = toc.sections.length;
+			if (sectionIndex === undefined) {
+				return i;
+			}
 
-	var sectionIndexes = {};
-	function getInsertionIndex(sectionIndex) {
-		var i = toc.sections.length;
-		if (sectionIndex === undefined) {
+			while (i > 0 && sectionIndexes[toc.sections[i-1]] > sectionIndex) {
+				i--;
+			}
+
 			return i;
 		}
 
-		while (i > 0 && sectionIndexes[toc.sections[i-1]] > sectionIndex) {
-			i--;
-		}
+		toc.add = function(section, title, index) {
+			sectionIndexes[section] = Number(index);
+			toc.sections.splice(getInsertionIndex(index), 0, section);
+			toc.titles[section] = title;
+		};
+		toc.reset = function() {
+			toc.sections = [];
+			toc.titles = {};
+		};
 
-		return i;
-	}
+		return toc;
+	}]);
 
-	toc.add = function(section, title, index) {
-		sectionIndexes[section] = Number(index);
-		toc.sections.splice(getInsertionIndex(index), 0, section);
-		toc.titles[section] = title;
-	};
-	toc.reset = function() {
-		toc.sections = [];
-		toc.titles = {};
-	};
+	app.directive('helpSection', ['help.toc', function(toc) {
+		return {
+			restict: 'A',
+			compile: function(elem, attrs) {
+				toc.add(attrs.helpSection, elem.html(), attrs.index);
+				elem.attr('id', attrs.helpSection);
+			}
+		};
+	}]);
 
-	return toc;
-}]);
+	app.directive('localizedSrc', ['help.lang', function(lang) {
+		return {
+			restict: 'A',
+			compile: function(elem, attrs) {
+				elem.attr('src', 'assets/help/img/' + lang + '/' + attrs.localizedSrc);
+			}
+		};
+	}]);
 
-app.directive('helpSection', ['help.toc', function(toc) {
-	return {
-		restict: 'A',
-		compile: function(elem, attrs) {
-			toc.add(attrs.helpSection, elem.html(), attrs.index);
-			elem.attr('id', attrs.helpSection);
-		}
-	};
-}]);
-
-app.directive('localizedSrc', ['help.lang', function(lang) {
-	return {
-		restict: 'A',
-		compile: function(elem, attrs) {
-			elem.attr('src', 'assets/help/img/' + lang + '/' + attrs.localizedSrc);
-		}
-	};
-}]);
-
-app.directive('wrapped', ['$compile', 'help.lang', function($compile, lang) {
-	return {
-		restrict: 'A',
-		replace: true,
-		link: function(scope, element, attrs) {
-			var url = 'assets/help/img/' + lang + '/' + attrs.url;
-			var clazz = attrs.class;
-			var html = '<div class="img-wrapper"><a href="' + url + '" target="_blank"><img src="' + url +  '" class="' + clazz + '"></a></div>';
-			element.replaceWith(html);
-			
-			
-		}
-	};
-}]);
+	app.directive('wrapped', ['$compile', 'help.lang', function($compile, lang) {
+		return {
+			restrict: 'A',
+			replace: true,
+			link: function(scope, element, attrs) {
+				var url = 'assets/help/img/' + lang + '/' + attrs.url;
+				var clazz = attrs.class;
+				var html = '<div class="img-wrapper"><a href="' + url + '" target="_blank"><img src="' + url +  '" class="' + clazz + '"></a></div>';
+				element.replaceWith(html);
+				
+				
+			}
+		};
+	}]);
+})();
