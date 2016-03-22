f8e2ab8228d4d6e51840b47d95069d9100bb0ae4
emhedenma <emhedenma@mycompany.com>
2016-03-22 08:53:29 +0000

    Wrapped code in IIFE.
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/unionvms-web@9990 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- app/partial/common/infoModal/infoModal.js
+++ app/partial/common/infoModal/infoModal.js
@@ -1,49 +1,53 @@
-var app = angular.module('unionvmsWeb');
+(function() {
+    'use strict';
 
-app.controller('infoModalCtrl', function($scope, $modalInstance, options, locale) {
+    var app = angular.module('unionvmsWeb');
 
-    $scope.loading = false;
-    $scope.labels = {
-        title: options.titleLabel || '',
-        text: options.textLabel || '',
-        close: options.closeLabel || locale.getString("common.close"),
-    };
+    app.controller('infoModalCtrl', function($scope, $modalInstance, options, locale) {
 
-    //Is there a promise that needs to be resolved before we have the text to show?
-    if(angular.isDefined(options.textLabelPromise)){
-        $scope.loading = true;
-        options.textLabelPromise.then(
-            function(textLabel){
-                $scope.loading = false;
-                $scope.labels.text = textLabel;
-            },
-            function(errorTextLabel){
-                $scope.loading = false;
-                $scope.labels.text = errorTextLabel;
-            }
-        );
-    }
+        $scope.loading = false;
+        $scope.labels = {
+            title: options.titleLabel || '',
+            text: options.textLabel || '',
+            close: options.closeLabel || locale.getString("common.close"),
+        };
 
-    $scope.cancel = function() {
-        $modalInstance.dismiss();
-    };
-});
-
-app.factory('infoModal',function($modal){
-    return {
-        open :function(options){
-            return $modal.open({
-                templateUrl: 'partial/common/infoModal/infoModal.html',
-                controller: 'infoModalCtrl',
-                windowClass : "infoModal",
-                size: "small",
-                resolve: {
-                    options: function() {
-                        return options || {};
-                    },
+        //Is there a promise that needs to be resolved before we have the text to show?
+        if(angular.isDefined(options.textLabelPromise)){
+            $scope.loading = true;
+            options.textLabelPromise.then(
+                function(textLabel){
+                    $scope.loading = false;
+                    $scope.labels.text = textLabel;
+                },
+                function(errorTextLabel){
+                    $scope.loading = false;
+                    $scope.labels.text = errorTextLabel;
                 }
-            });
+            );
         }
-    };
 
-});
+        $scope.cancel = function() {
+            $modalInstance.dismiss();
+        };
+    });
+
+    app.factory('infoModal',function($modal){
+        return {
+            open :function(options){
+                return $modal.open({
+                    templateUrl: 'partial/common/infoModal/infoModal.html',
+                    controller: 'infoModalCtrl',
+                    windowClass : "infoModal",
+                    size: "small",
+                    resolve: {
+                        options: function() {
+                            return options || {};
+                        },
+                    }
+                });
+            }
+        };
+
+    });
+})();
