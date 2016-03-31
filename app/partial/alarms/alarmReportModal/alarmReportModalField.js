d988a96ea80ea92a2b08a55e50f2e4cf7b14200a
emhedenma <emhedenma@mycompany.com>
2016-03-31 08:18:12 +0000

    Observe field value updates.
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/unionvms-web@10291 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- app/partial/alarms/alarmReportModal/alarmReportModalField.js
+++ app/partial/alarms/alarmReportModal/alarmReportModalField.js
@@ -14,11 +14,8 @@
 			},
 			template:
 				'<label>{{alarmReportField | i18n}}</label>' +
-				'<div ng-if="!missing" class="value">{{fieldValue}}</div>' +
-				'<div ng-if="missing" class="value missing">{{"alarms.position_report_information_value_missing" | i18n}}</div>',
-			controller: function($scope) {
-				$scope.missing = $scope.fieldValue === undefined;
-			}
+				'<div ng-if="fieldValue !== undefined" class="value">{{fieldValue}}</div>' +
+				'<div ng-if="fieldValue === undefined" class="value missing">{{"alarms.position_report_information_value_missing" | i18n}}</div>'
 		};
 	}
 
