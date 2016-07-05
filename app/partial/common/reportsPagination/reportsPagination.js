42430cea3c8d0bc02d2458038844bb6e23c11f52
emmatosjo <emmatosjo@mycompany.com>
2016-07-05 14:19:44 +0000

    SC - fixed bug on reportsPagination
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/unionvms-web@13278 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- app/partial/common/reportsPagination/reportsPagination.js
+++ app/partial/common/reportsPagination/reportsPagination.js
@@ -4,15 +4,37 @@ angular.module('unionvmsWeb').controller('ReportspaginationCtrl',function($scope
 
     //Handle change in page input field
     var inputChangeCallbackTimeout;
-    $scope.onInputPageChange = function(){
+    $scope.onInputPageChange = function(value){
+        if(value){
+            $scope.inputPage = value;
+        }
+        
+        if($scope.inputPage >= 1 && $scope.inputPage <= $scope.numPages && $scope.inputPage !== $scope.currentPage){
+            changeTimeout();
+        }else if(!angular.isDefined($scope.inputPage) || $scope.inputPage === null){
+            return;
+        }else if($scope.inputPage < 1){
+            $scope.inputPage = 1;
+            changeTimeout();
+        }else if($scope.inputPage > $scope.numPages){
+            $scope.inputPage = $scope.numPages;
+            changeTimeout();
+        }else{
+            $scope.inputPage = $scope.currentPage;
+        }
+    };
+
+    var changeTimeout = function(){
         $timeout.cancel(inputChangeCallbackTimeout);
         inputChangeCallbackTimeout = $timeout(function(){
-            if($scope.inputPage >= 1 && $scope.inputPage <= $scope.numPages && $scope.inputPage !== $scope.currentPage){
-                $scope.selectPage($scope.inputPage);
-            }else{
-                $scope.inputPage = $scope.currentPage;
-            }
+            $scope.selectPage($scope.inputPage);
         }, 300);
     };
+
+    $scope.$watch('currentPage',function(newVal,oldVal) {
+        if(newVal !== $scope.inputPage){
+            $scope.inputPage = newVal;
+        }
+    });
 
 });
