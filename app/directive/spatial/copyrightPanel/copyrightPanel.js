a0e9e2a37937fe59a68c548b5e54db478425d6d3
emmartihu <emmartihu@mycompany.com>
2016-10-11 13:21:08 +0000

    SC[UNIONVMS-1883] - Adding copyright notices and disclaimer for the layers that are shipped with the system
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/unionvms-web@13960 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- app/directive/spatial/copyrightPanel/copyrightPanel.js
+++ app/directive/spatial/copyrightPanel/copyrightPanel.js
@@ -1,10 +1,47 @@
-angular.module('unionvmsWeb').directive('copyrightPanel', function(mapService, layerPanelService) {
+angular.module('unionvmsWeb').directive('copyrightPanel', function(mapService, layerPanelService, locale) {
 	return {
 		restrict: 'EA',
 		replace: true,
 		scope: false,
 		templateUrl: 'directive/spatial/copyrightPanel/copyrightPanel.html',
-		controller: function(){
+		controller: function($scope){
+		    $scope.createTip = function(record){
+		        var target = angular.element('.disclaimer' + record.idx);
+		        var tip;
+		        if (angular.isDefined($(target).attr('data-hasqtip'))){
+		            tip = $(target);
+		        } else {
+		            tip = target.qtip({
+	                    content: {
+	                        text: function(evt, api){
+	                            return record.disclaimer;
+	                        }
+	                    },
+	                    position: {
+	                        my: 'left center',
+	                        at: 'right center',
+	                        target: target,
+	                        effect: false
+	                    },
+	                    show: {
+	                        solo: true,
+	                        when: false,
+	                        effect: false
+	                    },
+	                    style: {
+	                        classes: 'qtip-bootstrap disclaimer-tip'
+	                    }
+	                });
+		        }
+		        
+		        var api = tip.qtip('api');
+		        api.show();
+		    };
+		    
+		    $scope.setIdx = function(idx){
+		        $scope.records[idx].idx = idx;
+		    };
+		    
 		    this.init = function(){
 		        if (!angular.isDefined(mapService.map)){
 		            return;
@@ -16,10 +53,27 @@ angular.module('unionvmsWeb').directive('copyrightPanel', function(mapService, l
                 layers.forEach(function(layer, idx){
                     var attribution = layer.get('longAttribution');
                     if (layer.get('visible') && angular.isDefined(attribution) && attribution !== ''){
-                        records.push({
-                            title: layer.get('title'),
-                            copyright: layer.get('longAttribution')
-                        });
+                        if (attribution.indexOf('div') !== -1){
+                            //Support html content
+                            
+                            var record = {
+                                title: layer.get('title'),
+                                copyright: $(attribution).find('.copy-message').prop('outerHTML')
+                            }; 
+                            
+                            var disclaimerHtml = $(attribution).find('.copy-disclaimer');
+                            if (disclaimerHtml.length > 0){
+                                record.disclaimer = disclaimerHtml.prop('outerHTML');
+                            }
+                            records.push(record);
+                        } else {
+                            //Support normal text content
+                            records.push({
+                                title: layer.get('title'),
+                                copyright: attribution
+                            });
+                        }
+                        
                     }
                 }, this);
                 
@@ -36,10 +90,6 @@ angular.module('unionvmsWeb').directive('copyrightPanel', function(mapService, l
             layerPanelService.panelToReload.push(function(){
                 scope.records = ctrl.init();
             });
-
-            /*scope.$on('reloadLegend', function(){
-                scope.records = ctrl.init();
-            });*/
         }
 	};
 });
