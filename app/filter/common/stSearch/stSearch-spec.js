f5aa382d7c44804f83fa0bf7d944d1a07b5df293
embrzyspi <embrzyspi@mycompany.com>
2016-10-27 08:24:44 +0000

    test search created
    git-svn-id: https://webgate.ec.europa.eu/CITnet/svn/UNIONVMS/branches/dev/unionvms-web@14205 ffa9caa6-9a2d-11e4-8c71-fd203efb3018

--- app/filter/common/stSearch/stSearch-spec.js
+++ app/filter/common/stSearch/stSearch-spec.js
@@ -1,13 +1,83 @@
 describe('stSearch', function() {
 
+	var filter;
+	
 	beforeEach(module('unionvmsWeb'));
-
-	it('should ...', inject(function($filter) {
-
-        var filter = $filter('stSearch');
-
-		//expect(filter('input')).toEqual('output');
-
+	
+	beforeEach(inject(function($filter){
+		filter = $filter('stSearch');
 	}));
+	
+	function makeArray(){
+		return [{
+			createdBy: "rep_power",
+			createdOn: "2016-10-26T08:27:54",
+			desc: "r1 description",
+			executedOn: "2016-10-26T09:01:38",
+			name: "r11"
+		},{
+			createdBy: "rep_power",
+			createdOn: "2016-10-26T08:27:54",
+			desc: "r1 description",
+			executedOn: "2016-10-26T09:01:38",
+			name: "r1"
+		},{
+			createdBy: "rep_power",
+			createdOn: "2016-10-26T08:27:54",
+			desc: "r2 description",
+			executedOn: "2016-10-26T08:28:16",
+			name: "r2"
+		}];
+	}
+
+	function makePredictedObj(searchStr){
+		return "name|desc|createdOn|createdBy|executedOn|visibility|" + searchStr;
+	}	
+	
+	it('should return filtered results with multiple matches', function() {
+		var srcData = makeArray();
+		var predictedObj = makePredictedObj("r1");
+		var result = [{
+			createdBy: "rep_power",
+			createdOn: "2016-10-26T08:27:54",
+			desc: "r1 description",
+			executedOn: "2016-10-26T09:01:38",
+			name: "r11"
+		},{
+			createdBy: "rep_power",
+			createdOn: "2016-10-26T08:27:54",
+			desc: "r1 description",
+			executedOn: "2016-10-26T09:01:38",
+			name: "r1"
+		}];
+        
+        expect(filter(srcData, predictedObj)).toEqual(result);
+		expect(filter(srcData, predictedObj)).toEqual(jasmine.any(Array));
+		expect(filter(srcData, predictedObj).length).toEqual(2);
+	});
+	
+	it('should return filtered results with a single result', function() {
+		var srcData = makeArray();
+		var predictedObj = makePredictedObj("r2");
+		var result = [{
+			createdBy: "rep_power",
+			createdOn: "2016-10-26T08:27:54",
+			desc: "r2 description",
+			executedOn: "2016-10-26T08:28:16",
+			name: "r2"
+		}];
+        
+        expect(filter(srcData, predictedObj)).toEqual(result);
+		expect(filter(srcData, predictedObj)).toEqual(jasmine.any(Array));
+		expect(filter(srcData, predictedObj).length).toEqual(1);
+	});
+	
+	it('should return empty array if input search string is not found in the data', function() {
+		var srcData = makeArray();
+		var predictedObj = makePredictedObj("testing");
+        
+		expect(filter(srcData, predictedObj)).toEqual(jasmine.any(Array));
+		expect(filter(srcData, predictedObj).length).toEqual(0);
+	});
 
 });
