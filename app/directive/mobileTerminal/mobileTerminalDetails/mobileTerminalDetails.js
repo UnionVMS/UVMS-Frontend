30177d8a52e0cd45b1e8f916815818ce2329d11c
Sandra Svensson <sandra.svensson@havochvatten.se>
2016-11-09 16:49:31 +0100

    [UVMS-3078] * Display mobileterminal directive on vessel page * Updates to directive

--- app/directive/mobileTerminal/mobileTerminalDetails/mobileTerminalDetails.js
+++ app/directive/mobileTerminal/mobileTerminalDetails/mobileTerminalDetails.js
@@ -18,8 +18,9 @@ angular.module('unionvmsWeb')
         require: "^ngModel",
         scope: {
             ngModel : '=',
-            mobileTerminals : '=',
-            transporderSystems : '='
+            mobileTerminal : '=',
+            transporderSystems : '=', 
+            modeltype: '='
         },
 		templateUrl: 'directive/mobileTerminal/mobileTerminalDetails/mobileTerminalDetails.html',
 		link: function(scope, element, attrs, fn) {
@@ -29,6 +30,90 @@ angular.module('unionvmsWeb')
 });
 
 angular.module('unionvmsWeb')
-    .controller('mobileTerminalDetailsCtrl', function($scope, locale){
+    .controller('mobileTerminalDetailsCtrl', function($scope, $location, $filter, locale, SystemTypeAndPlugin, configurationService, MobileTerminalHistoryModal){
+
+        $scope.transponderSystems = [];
+
+        var init = function(){
+             //Get list transponder systems
+            if(angular.isDefined(configurationService.getConfig('MOBILE_TERMINAL_TRANSPONDERS'))){
+                $scope.transpondersConfig = configurationService.getConfig('MOBILE_TERMINAL_TRANSPONDERS');
+                $scope.createTransponderSystemDropdownOptions();
+            }else{
+                alertService.showErrorMessage(locale.getString('mobileTerminal.add_new_alert_message_on_load_transponders_error'));
+            }
+
+            if(angular.isDefined($scope.mobileTerminal) && angular.isDefined($scope.mobileTerminal.plugin)){
+                $scope.typeAndPlugin = $scope.getModelValueForTransponderSystemBySystemTypeAndPlugin($scope.mobileTerminal.type, $scope.mobileTerminal.plugin.labelName, $scope.mobileTerminal.plugin.serviceName);
+            }
+        };
+
+        //Disable form
+        $scope.disableForm = function() {
+            if ($scope.modeltype === 'VESSEL') {
+                return true;
+            }
+            return false;
+        }
+
+        //Get terminal config for the selected terminal type
+        $scope.getTerminalConfig = function(){
+            if(angular.isDefined($scope.mobileTerminal)){
+                var systemName = $scope.mobileTerminal.type;
+                return $scope.transpondersConfig.getTerminalConfigBySystemName(systemName);
+            }
+        };
+
+        //Get model value for the transponder system dropdown by system type and plugin
+        $scope.getModelValueForTransponderSystemBySystemTypeAndPlugin = function(type, labelName, serviceName){
+            var value, tmp;
+            $.each($scope.transponderSystems, function(index, system){
+                var systemAndTypeAndPluginItem = system.typeAndPlugin;
+                tmp = new SystemTypeAndPlugin(type, labelName, serviceName);
+                if(systemAndTypeAndPluginItem.equals(tmp)){
+                    value = systemAndTypeAndPluginItem;
+                    return false;
+                }
+            });
+            return value;
+        };
+
+        //Create dropdown for transponder system
+        $scope.createTransponderSystemDropdownOptions = function(){
+            //Create dropdown values
+            $.each($scope.transpondersConfig.terminalConfigs, function(key, config){
+                //LES capability
+                if(config.capabilities["PLUGIN"] && _.isArray(config.capabilities["PLUGIN"])){
+                    $.each(config.capabilities["PLUGIN"], function(key2, pluginOption){
+                        $scope.transponderSystems.push({text : config.viewName +" : " +pluginOption.text, typeAndPlugin : new SystemTypeAndPlugin(config.systemType, pluginOption.attributes['LABELNAME'], pluginOption.attributes['SERVICENAME'])});
+                    });
+                }else{
+                    $scope.transponderSystems.push({text : config.viewName, typeAndPlugin : new SystemTypeAndPlugin(config.systemType, undefined)});
+                }
+            });
+        };
+
+        //Open history modal
+        $scope.onMobileTerminalHistoryClick = function(){
+            MobileTerminalHistoryModal.show($scope.mobileTerminal);
+        };
+
+        //Edit current mobile terminal
+        $scope.editMobileTerminalDetails = function(){
+            $location.path('/communication/' + $scope.mobileTerminal.guid);
+        };    
+
+        $scope.menuBarFunctions = {
+            historyCallback: $scope.onMobileTerminalHistoryClick,
+            showHistory: function(mobileTerminal) {
+                return true;
+            },
+            editCallback: $scope.editMobileTerminalDetails,
+            showEdit: function(mobileTerminal) {
+                return true;
+            }
+        };
+
+        init();
     }
 );
