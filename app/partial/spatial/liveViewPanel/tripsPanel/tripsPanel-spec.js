4340ebca0502e23c11a47a464ce907f6dfc66555
MATOS Joao <Joao.Matos@arhs-dev-be.com>
2016-11-16 18:47:09 +0100

    SC[UNIONVMS-1921] - added unit tests

--- app/partial/spatial/liveViewPanel/tripsPanel/tripsPanel-spec.js
+++ app/partial/spatial/liveViewPanel/tripsPanel/tripsPanel-spec.js
@@ -1,18 +1,73 @@
 describe('TripspanelCtrl', function() {
 
-	/*beforeEach(module('unionvmsWeb'));
+	beforeEach(module('unionvmsWeb'));
 
-	var scope,ctrl;
+	var scope,ctrl,spatialConfigAlertService,genMapServSpy,tripSumSpy,Trip;
 
-    beforeEach(inject(function($rootScope, $controller) {
-      scope = $rootScope.$new();
-      ctrl = $controller('TripspanelCtrl', {$scope: scope});
-    }));	
+	beforeEach(function(){
+			tripSumSpy = jasmine.createSpyObj('tripSummaryService',['resetMapConfigs']);
+			genMapServSpy = jasmine.createSpyObj('genericMapService',['setMapBasicConfigs']);
+			
+			module(function($provide){
+					$provide.value('genericMapService', genMapServSpy);
+					$provide.value('tripSummaryService', tripSumSpy);
+			});
+		});
+
+	beforeEach(inject(function($httpBackend) {
+		//Mock
+		$httpBackend.whenGET(/usm/).respond();
+		$httpBackend.whenGET(/i18n/).respond();
+		$httpBackend.whenGET(/globals/).respond({data : []});
+	}));
+
+	beforeEach(inject(function(_spatialConfigAlertService_, _Trip_, _tripSummaryService_) {
+		spatialConfigAlertService = _spatialConfigAlertService_;
+		Trip = _Trip_;
+	}));
+
+	beforeEach(inject(function($rootScope, $controller) {
+		buildMocks();
+		scope = $rootScope.$new();
+		ctrl = $controller('TripspanelCtrl', {$scope: scope, spatialConfigAlertService: spatialConfigAlertService});
+	}));
+
+	function buildMocks(){
+			tripSumSpy.resetMapConfigs.andCallFake(function(){
+				return {
+						then: function(callback){
+								return callback(resetMapConfigs());
+						}
+				};
+			});
+
+			genMapServSpy.setMapBasicConfigs.andCallFake(function(){
+				return {
+						then: function(callback){
+								return callback(setMapBasicConfigs());
+						}
+				};
+			});
+	}
+
+	function resetMapConfigs(){
+			return true;
+	}
+
+	function setMapBasicConfigs(){
+			return true;
+	}
 
 	it('should ...', inject(function() {
+		scope.$digest();
 
-		expect(1).toEqual(1);
+		scope.tripSummServ.initializeTrip(0);
+		//scope.tripSummServ.isLoadingTrip = true;
+		scope.$digest();
+
+		scope.tripSummServ.closeTab(0);
+		scope.$digest();
 		
-	}));*/
+	}));
 
 });
