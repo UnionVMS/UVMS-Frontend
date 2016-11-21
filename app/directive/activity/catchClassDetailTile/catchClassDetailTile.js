aedcfd64d0c6b7ab6781e774e2b1f2e25ac2f2d0
MARTINS Hugo <Hugo.Martins@arhs-dev-be.com>
2016-11-21 18:31:38 +0100

    SC[UNIONVMS-1533] Adding stacked bar chart to the catchClassDetailTile

--- app/directive/activity/catchClassDetailTile/catchClassDetailTile.js
+++ app/directive/activity/catchClassDetailTile/catchClassDetailTile.js
@@ -40,8 +40,84 @@ controller('CatchClassDetailTileCtrl', function($scope, locale){
         $scope.selected.total = parseFloat($scope.selected.lsc) + parseFloat($scope.selected.bms);
         var title = 'activity.location';
         if ($scope.selected.locations.length > 1){
-            title += 's'
+            title += 's';
         }
         $scope.locationTitle = locale.getString(title);
+        
+        generateChartData();
+    };
+    
+    function generateChartData(){
+        var colors = palette('tol-rainbow', 2);
+        var lscSeries = {
+            key: locale.getString('activity.lsc').toUpperCase(),
+            color: colors[0],
+            values: []
+        };
+        
+        var bmsSeries = {
+            key: locale.getString('activity.bms').toUpperCase(),
+            color: colors[1],
+            values: []
+        };
+        
+        var counter = 0;
+        angular.forEach($scope.ngModel, function(item){
+            lscSeries.values.push(generateChartRecord(item, 'lsc', counter));
+            bmsSeries.values.push(generateChartRecord(item, 'bms', counter));
+            counter += 1;
+        });
+        
+        $scope.chartData = [lscSeries, bmsSeries];
+    }
+    
+    function generateChartRecord (data, type, idx){
+        return {
+            idx: idx,
+            key: data.species,
+            series: type === 'lms' ? 0 : 1,
+            x: idx,
+            y: data[type]
+        };
+    } 
+    
+    $scope.options = {
+        chart: {
+            type: 'multiBarChart',
+            stacked: true,
+            showLegend: true,
+            showControls: false,
+            useInteractiveGuideline: true,
+            yAxis: {
+                axisLabel: locale.getString('activity.header_fa_weight'),
+                showMaxMin: false,
+                ticks: 5
+            },
+            xAxis: {
+                rotateYLabel: true,
+                rotateLabels: -45,
+                tickFormat: function(xValue){
+                    return $scope.ngModel[xValue].species;
+                }
+            },
+            interactiveLayer: {
+                tooltip: {
+                    headerFormatter: function (xValue) {
+                        return $scope.ngModel[xValue].species;
+                    },
+                    contentGenerator: function(data){
+                        var html = '<table class="catch-class-detail-chart"><thead><tr><td colspan="3"><strong>SOL</strong></td></tr></thead><tbody>';
+                        for (var i = 0; i < data.series.length; i++){
+                            html += '<tr><td class="legend-color-guide"><div style="background-color: #' + data.series[i].color + '"></div></td>'
+                            html += '<td class="key">' + data.series[i].key + '</td>';
+                            html += '<td class="value">' + data.series[i].value + ' kg' + '</td></tr>';
+                        }
+                        html += '</tbody></table';
+                        
+                        return html;
+                    }
+                }
+            }
+        }        
     };
 });
