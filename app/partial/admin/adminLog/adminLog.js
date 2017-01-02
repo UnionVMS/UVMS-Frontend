663a14f41abc1ce23af3ec03ea16702e6e82fab0
Sandra Svensson <sandra.svensson@havochvatten.se>
2017-01-02 13:33:10 +0100

    [UVMS-2008] * Add URL for "Program Polls"

--- app/partial/admin/adminLog/adminLog.js
+++ app/partial/admin/adminLog/adminLog.js
@@ -174,6 +174,8 @@ angular.module('unionvmsWeb').controller('AuditlogCtrl', function($scope, $q, $f
                     return "/assets/" + audit.affectedObject;
                 case TYPES.ASSETS_AND_TERMINALS.POLL:
                     return "/polling/logs/" + audit.affectedObject;
+                case TYPES.ASSETS_AND_TERMINALS.POLLING_PROGRAM:
+                    return "/polling/logs/" + audit.affectedObject;
                 case TYPES.ALARMS.ALARM:
                     return "/alerts/holdingtable/" + audit.affectedObject;
                 case TYPES.ALARMS.TICKET:
