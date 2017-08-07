b8ba1b28645a297105a413170e45bad420cb87cc
MARTINS Hugo <Hugo.Martins@arhs-dev-be.com>
2017-08-07 16:49:04 +0200

    [UNIONVMS-2929] Implement new fields in the exchange search form (status, type and source)

--- app/directive/common/search/advancedSearch/exchange/exchangeSearch.js
+++ app/directive/common/search/advancedSearch/exchange/exchangeSearch.js
@@ -14,7 +14,69 @@ angular.module('unionvmsWeb').controller('ExchangeSearchController', function($s
 	var init = function(){
 	    $scope.advancedSearchObject.EXCHANGE_TIME_SPAN = $scope.DATE_TODAY;
         $scope.resetSearch();
-        $scope.recipientItems = configurationService.setTextAndCodeForDropDown(configurationService.getValue('EXCHANGE', 'RECIPIENT'), 'RECIPIENT', 'EXCHANGE', true);
+        //FIXME the following should be uncommented when exchange config rest service is updated
+        //Do not forget to check config.lang.json
+//        $scope.recipientItems = configurationService.setTextAndCodeForDropDown(configurationService.getValue('EXCHANGE', 'RECIPIENT'), 'RECIPIENT', 'EXCHANGE', true);
+//        $scope.statusItems = configurationService.setTextAndCodeForDropDown(configurationService.getValue('EXCHANGE', 'STATUS'), 'STATUS', 'EXCHANGE', true);
+        
+        //FIXME to remove all items mocked here
+        $scope.recipientItems = [{
+            code: 'DNK', text: 'DNK'
+        },{
+            code: 'FIN', text: 'FIN'
+        }];
+        
+        $scope.statusItems = [{
+            code: 'EXCHANGE_STATUS_ISSUED', text: 'Issued'
+        },{
+            code: 'EXCHANGE_STATUS_PENDING', text: 'Pending'
+        },{
+            code: 'EXCHANGE_STATUS_PROBABLY_TRANSMITTED', text: 'Probably transmitted'
+        },{
+            code: 'EXCHANGE_STATUS_FAILED', text: 'Failed'
+        },{
+            code: 'EXCHANGE_STATUS_SUCCESSFUL', text: 'Successful'
+        },{
+            code: 'EXCHANGE_STATUS_SUCCESSFUL_WITH_WARNINGS', text: 'Successful with warnings'
+        },{
+            code: 'EXCHANGE_STATUS_UNKNOWN', text: 'Unknown'
+        },{
+            code: 'EXCHANGE_STATUS_OK', text: 'OK'
+        },{
+            code: 'EXCHANGE_STATUS_WARN', text: 'Warnings'
+        },{
+            code: 'EXCHANGE_STATUS_ERROR', text: 'Error'
+        },{
+            code: 'EXCHANGE_STATUS_SENT', text: 'Sent'
+        }];
+        
+        $scope.sourceItems = [{
+            code: 'Inmarsat-C', text: 'Inmarsat-C'
+        },{
+            code: 'FLUX', text: 'FLUX'    
+        }];
+        
+        $scope.typeItems = [{
+            code: 'MOVEMENT', text: 'Movement'
+        },{
+            code: 'POLL', text: 'Poll'
+        },{
+            code: 'ALARM', text: 'Alarm'
+        },{
+            code: 'UNKNOWN', text: 'Unknown'
+        },{
+            code: 'SALES_QUERY', text: 'Sales query'
+        },{
+            code: 'SALES_REPORT', text: 'Sales report'
+        },{
+            code: 'SALES_RESPONSE', text: 'Sales response'
+        },{
+            code: 'FA_QUERY', text: 'FA query'
+        },{
+            code: 'FA_REPORT', text: 'FA report'
+        },{
+            code: 'FA_RESPONSE', text: 'FA response'
+        }];
     };
 
     $scope.$on("resetExchangeLogSearch", function() {
