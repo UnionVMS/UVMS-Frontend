7d467096549be706737a5412f948d2bd28cc5a43
stihft <stijn.hooft@lv.vlaanderen.be>
2017-09-14 12:22:37 +0200

    Fill up trip data in sales details Sprint Backlog Item #140316 - Tripbalk opvullen met data uit FA

--- app/partial/sales/salesDetails/fishingTrip/fishingTrip.js
+++ app/partial/sales/salesDetails/fishingTrip/fishingTrip.js
@@ -12,15 +12,18 @@
             }
         });
 
-    function fishingTripCtrl(userService) {
+    function fishingTripCtrl(userService, salesFishingActivityService) {
         /* jshint validthis:true */
         var vm = this;
 
-        //HANPOP temp code untill activies are ready.
-        vm.trip.departureLocation = 'TODO';
-        vm.trip.departureDate = new Date();
-        vm.trip.arrivalLocation = 'TODO';
-        vm.trip.arrivalDate = new Date();
+        salesFishingActivityService.findFishingActivity("DEPARTURE", vm.trip.extId).then(function(result) {
+            vm.trip.departureLocation = result.port;
+            vm.trip.departureDate = result.date;
+        });
+        salesFishingActivityService.findFishingActivity("ARRIVAL", vm.trip.extId).then(function(result) {
+            vm.trip.arrivalLocation = result.port;
+            vm.trip.arrivalDate = result.date;
+        });
 
         vm.navigateToVesselAllowed = userService.isAllowed('viewVesselsAndMobileTerminals', 'Union-VMS', true);
     }
