f318d8e770e295df420356b90ebfcdc1056daaaa
jjgenio <jgenio2@gmail.com>
2017-11-17 10:17:23 +0100

    SC - fixing trips list sorting

--- app/partial/activity/tripReportsList/tripReportsList.js
+++ app/partial/activity/tripReportsList/tripReportsList.js
@@ -9,13 +9,19 @@ angular.module('unionvmsWeb').controller('TripreportslistCtrl',function($scope,r
     $scope.attrVisibility = visibilityService;
     $scope.tripSummServ = tripSummaryService;
     $scope.activityTypes = [];
+    var stLastTableState;
 
     $scope.updateTripsList = function(tableState, ctrl){
+      delete tableState.isSorting;
+      if(stLastTableState && !_.isEqual(tableState.sort, stLastTableState.sort)){
+        tableState.isSorting = true;
+      }
+      stLastTableState = angular.copy(tableState);
       $scope.callServer(tableState, ctrl, 'tripsList');
     };
 
     $scope.openTripSummary = function(tripId){
       $scope.tripSummServ.openNewTrip(tripId);
       $scope.goToView(3);
-   };
+    };
 });
