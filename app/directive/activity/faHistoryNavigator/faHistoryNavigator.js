a21df468741cbfedc7e2f9d11652c22dfe482b75
MARTINS Hugo <Hugo.Martins@arhs-dev-be.com>
2017-11-21 16:05:34 +0100

    Bugfixing fishing activity history navigation^

--- app/directive/activity/faHistoryNavigator/faHistoryNavigator.js
+++ app/directive/activity/faHistoryNavigator/faHistoryNavigator.js
@@ -13,10 +13,14 @@ copy of the GNU General Public License along with the IFDM Suite. If not, see <h
  * @memberof unionvmsWeb
  * @ngdoc directive
  * @name faHistoryNavigator
+ * @param fishingActivityService {Service} The fishing activity service <p>{@link unionvmsWeb.fishingActivityService}</p>
+ * @param FishingActivity {Model} The model of a fishing activity
+ * @attr {Object} history - An object containing the ids of the previous and next activity in the history
+ * @attr {String} activityName - The activity type name so that the proper rest service can be called
  * @description
- *  A reusable tile that will display the history of the current fishing activity.
+ *  A reusable button that will allow to navigate through the history of a particular fishing activity.
  */
-angular.module('unionvmsWeb').directive('faHistoryNavigator', function (fishingActivityService,FishingActivity) {
+angular.module('unionvmsWeb').directive('faHistoryNavigator', function (fishingActivityService,FishingActivity, $compile) {
     return {
         restrict: 'E',
         replace: true,
@@ -27,9 +31,58 @@ angular.module('unionvmsWeb').directive('faHistoryNavigator', function (fishingA
         templateUrl: 'directive/activity/faHistoryNavigator/faHistoryNavigator.html',
         link: function (scope, element, attrs, fn) {
             scope.faServ = fishingActivityService;
-            scope.openHistoryView = function (activityId) {
-                scope.faServ.getFishingActivity(new FishingActivity(scope.activityName),undefined,activityId);  
+            
+            scope.status = {
+                id: undefined,
+                documentType: undefined,
+                activityType: undefined
             };
+            
+            /**
+             * Load the activity from a previous/next version id
+             * 
+             * @memberof faHistoryNavigator
+             * @public
+             * @param {Number} activityId - The id of the activity to be loaded
+             */
+            scope.openHistoryView = function (activityId) {
+                scope.status.id = activityId;
+                scope.status.documentType = scope.faServ.documentType;
+                scope.status.activityType = scope.faServ.activityType;
+              
+                scope.faServ.resetActivity();
+                scope.faServ.getFishingActivity(new FishingActivity(scope.activityName),scope.recompileView,activityId);  
+            };
+            
+            /**
+             * Recompile the activity view and update the fishing activity service
+             * 
+             * @memberof faHistoryNavigator
+             * @public
+             */
+            scope.recompileView = function(){
+                scope.faServ.id = scope.status.id;
+                scope.faServ.documentType = scope.status.documentType;
+                scope.faServ.activityType = scope.status.activityType;
+                var content = angular.element('.activity-details');
+                $compile(content.contents())(scope);
+            };
+            
+            /**
+             * Update the correction status of the activity service according to the history items provided by the REST service
+             * 
+             * @memberof faHistoryNavigator
+             * @public
+             */
+            scope.updateCorrection = function(){
+                var isCorrection = true;
+                if (!angular.isDefined(scope.history.previousId) || scope.history.previousId === 0){
+                    isCorrection = false;
+                }
+                scope.faServ.isCorrection = isCorrection;
+            };
+          
+            scope.updateCorrection();
         }
     };
 });
