a06cddb8bcbb1989bc3c5703ebb84d6e3963b1de
jjgenio <jgenio2@gmail.com>
2017-11-30 18:05:39 +0100

    SC - fixes in unit test

--- app/partial/subscriptions/newSubscription/repParamSubscription/repParamSubscription-spec.js
+++ app/partial/subscriptions/newSubscription/repParamSubscription/repParamSubscription-spec.js
@@ -4,11 +4,18 @@ describe('RepparamsubscriptionCtrl', function() {
 
 	var scope/* ,ctrl */;
 
-		beforeEach(inject(function($rootScope, $controller, Subscription) {
-			scope = $rootScope.$new();
-			scope.subscription = new Subscription();
-			/* ctrl = $controller('RepparamsubscriptionCtrl', {$scope: scope}); */
-		}));	
+	beforeEach(inject(function($rootScope, $controller, Subscription) {
+		scope = $rootScope.$new();
+		scope.subscription = new Subscription();
+		/* ctrl = $controller('RepparamsubscriptionCtrl', {$scope: scope}); */
+	}));
+
+	beforeEach(inject(function($httpBackend) {
+		//Mock
+		$httpBackend.whenGET(/usm/).respond();
+		$httpBackend.whenGET(/i18n/).respond();
+		$httpBackend.whenGET(/globals/).respond({data : []});
+	}));
 
 	it('should initialize the creation type as "Transport original report"', inject(function($compile) {
 		expect(scope.subscription.reportParams.vesselIdType.type).toEqual('original');
