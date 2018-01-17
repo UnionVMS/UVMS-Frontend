9a598a339254a5e8e6cdfbf26067152f511fedde
MathiasBulte <bulte.mathias@gmail.com>
2018-01-17 11:03:20 +0100

    Task #146741: Performance improvement SearchReportsHelper.enrichWithVesselInformation

--- app/directive/sales/simpleExport/simpleExport.js
+++ app/directive/sales/simpleExport/simpleExport.js
@@ -5,6 +5,7 @@ angular.module('unionvmsWeb').directive('simpleExport', function () {
         controller: 'SimpleExportCtrl',
         scope: {
             exportfunc: "=",
+            amountOfReports: "="
         },
         templateUrl: 'directive/sales/simpleExport/simpleExport.html',
         link: function (scope, element, attrs, fn) {
@@ -14,8 +15,13 @@ angular.module('unionvmsWeb').directive('simpleExport', function () {
 
 angular.module('unionvmsWeb')
 .controller('SimpleExportCtrl', function ($scope, searchService, locale, csvService, salesCsvService) {
+    const MAX_ITEMS = 1000;
+
     $scope.loading = false;
     $scope.errors = [];
+    $scope.confirmed = false;
+    $scope.clicked = false;
+
     $scope.performExport = function () {
         $scope.loading = true;
         var searchObj = searchService.getAdvancedSearchObject();
@@ -23,6 +29,34 @@ angular.module('unionvmsWeb')
             exportFinished(data);
         }, exportFailed);
     };
+
+    $scope.needsConfirmation = function () {
+      return $scope.amountOfReports > MAX_ITEMS;
+    } ;
+
+    $scope.export = function () {
+        if ($scope.needsConfirmation()) {
+            //Show a message on the first click when 'export all' will be limited to 1000 records
+            if (!$scope.confirmed) {
+                $('#popoverbutton').popover('show');
+            }
+
+            //When the user clicks confirm, reset variables and trigger the export
+            if (!$scope.confirmed && $scope.clicked) {
+                $scope.confirmed = false;
+                $scope.clicked = false;
+                $('#popoverbutton').popover('hide');
+                $scope.performExport();
+            } else {
+                //Only set clicked when we haven't triggered the export
+                $scope.clicked = true;
+            }
+        } else {
+            $scope.performExport();
+        }
+    };
+
+    $('[data-toggle="popover"]').popover();
 
 
     function exportFinished(data) {
