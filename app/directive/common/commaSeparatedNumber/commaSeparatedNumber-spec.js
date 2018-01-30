f80717f4813f15ded414624afe41e9ae978bd3e2
Hugo Martins <Hugo.Martins@arhs-dev-be.com>
2018-01-30 10:53:20 +0100

    Adding tests to subscriptions

--- app/directive/common/commaSeparatedNumber/commaSeparatedNumber-spec.js
+++ app/directive/common/commaSeparatedNumber/commaSeparatedNumber-spec.js
@@ -2,22 +2,47 @@ describe('commaSeparatedNumber', function() {
 
   beforeEach(module('unionvmsWeb'));
 
-  var scope,compile;
+  var scope,compile, elem, ngModelCtrl;
 
-  beforeEach(inject(function($rootScope,$compile) {
+  beforeEach(inject(function($rootScope,$compile, $httpBackend) {
     scope = $rootScope.$new();
+    scope.data = '';
     compile = $compile;
+
+    $httpBackend.whenGET(/usm/).respond();
+    $httpBackend.whenGET(/i18n/).respond();
+    $httpBackend.whenGET(/globals/).respond({data: []});
+
+    elem = compile('<input id="test-csn" type="text" ng-model="data" comma-separated-number></input>')(scope);
+    elem.appendTo('body');
+    ngModelCtrl = elem.controller('ngModel');
   }));
 
-  it('should ...', function() {
-
-    /* 
-    To test your directive, you need to create some html that would use your directive,
-    send that through compile() then compare the results.
-
-    var element = compile('<div mydirective name="name">hi</div>')(scope);
-    expect(element.text()).toBe('hello, world');
-    */
-
+  afterEach(function() {
+    angular.element('test-csn').remove();
   });
+
+  it('should not accept two consecutive commas', function() {
+      ngModelCtrl.$setViewValue('1,,2');
+      scope.$digest();
+
+      expect(angular.element('#test-csn').length).toBe(1);
+      expect(elem.val()).toEqual('1,2');
+  });
+
+   it('should not accept a comma in the beginning', function() {
+      ngModelCtrl.$setViewValue(',1,2');
+      scope.$digest();
+
+      expect(angular.element('#test-csn').length).toBe(1);
+      expect(elem.val()).toEqual('1,2');
+   });
+
+   it('should not accept non digit characters', function() {
+      ngModelCtrl.$setViewValue('1,a,b,c,2');
+      scope.$digest();
+
+      expect(angular.element('#test-csn').length).toBe(1);
+      expect(elem.val()).toEqual('1,2');
+   });
 });
--- app/partial/subscriptions/subscriptionsTopAlert/subscriptionsTopAlert-spec.js
+++ app/partial/subscriptions/subscriptionsTopAlert/subscriptionsTopAlert-spec.js
@@ -2,17 +2,28 @@ describe('SubscriptionstopalertCtrl', function() {
 
     beforeEach(module('unionvmsWeb'));
 
-    var scope,ctrl;
+    var scope,ctrl, mockSubServ;
 
-    beforeEach(inject(function($rootScope, $controller) {
+    beforeEach(function () {
+       mockSubServ = jasmine.createSpyObj('subscriptionsService', ['resetAlert']);
+
+       module(function ($provide) {
+           $provide.value('subscriptionsService', mockSubServ);
+       })
+    });
+
+    beforeEach(inject(function($rootScope, $controller, $httpBackend) {
       scope = $rootScope.$new();
       ctrl = $controller('SubscriptionstopalertCtrl', {$scope: scope});
+
+      $httpBackend.whenGET(/usm/).respond();
+      $httpBackend.whenGET(/i18n/).respond();
+      $httpBackend.whenGET(/globals/).respond({data : []});
     }));
 
-    it('should ...', inject(function() {
-
-        expect(1).toEqual(1);
-        
+    it('should close the subscriptions top alert', inject(function() {
+        scope.close();
+        expect(mockSubServ.resetAlert).toHaveBeenCalled();
     }));
 
 });
