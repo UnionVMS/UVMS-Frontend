99b022bbc7a5428fb44023c78708f18182fd4d29
Sumeela Anna-Ranga <annarasu@aris-lux.lan>
2018-02-01 17:07:39 +0100

    SC[UNIONVMS-3592]- added documentation and changed the invalid trip message

--- app/partial/activity/tripReportsList/tripReportsList.js
+++ app/partial/activity/tripReportsList/tripReportsList.js
@@ -1,4 +1,27 @@
-angular.module('unionvmsWeb').controller('TripreportslistCtrl',function($scope,tripSummaryService,visibilityService,$stateParams){
+/*
+Developed with the contribution of the European Commission - Directorate General for Maritime Affairs and Fisheries
+© European Union, 2015-2016.
+
+This file is part of the Integrated Fisheries Data Management (IFDM) Suite. The IFDM Suite is free software: you can
+redistribute it and/or modify it under the terms of the GNU General Public License as published by the
+Free Software Foundation, either version 3 of the License, or any later version. The IFDM Suite is distributed in
+the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a
+copy of the GNU General Public License along with the IFDM Suite. If not, see <http://www.gnu.org/licenses/>.
+*/
+/**
+ * @memberof unionvmsWeb
+ * @ngdoc controller
+ * @name ActivityreportslistCtrl
+ * @param $scope {Service} controller scope
+ * @param visibilityService {Service} The visibility service <p>{@link unionvmsWeb.visibilityService}</p>
+ * @param tripSummaryService {Service} the trip summary service <p>{@link unionvmsWeb.tripSummaryService}</p>
+ * @param $stateParams {Service} the angular ui router state params service
+ * @attr {Array} displayedTrips - The array of displayed trips used by smart tables
+ * @description
+ *  The controller for the fisihing trips reports table list
+ */
+angular.module('unionvmsWeb').controller('TripreportslistCtrl',function($scope, tripSummaryService, visibilityService, $stateParams){
 
     $scope.isTripFilterVisible = false;
     $scope.startDate = undefined;
