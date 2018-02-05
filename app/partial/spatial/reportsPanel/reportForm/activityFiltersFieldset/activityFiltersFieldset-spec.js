95fa814039a72854b22515fd06cd7feab98f9740
Sumeela Anna-Ranga <annarasu@aris-lux.lan>
2018-02-05 11:01:40 +0100

    service integration of combo boxes in fishing activity filter.

--- app/partial/spatial/reportsPanel/reportForm/activityFiltersFieldset/activityFiltersFieldset-spec.js
+++ app/partial/spatial/reportsPanel/reportForm/activityFiltersFieldset/activityFiltersFieldset-spec.js
@@ -9,43 +9,75 @@ the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the impl
 FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a
 copy of the GNU General Public License along with the IFDM Suite. If not, see <http://www.gnu.org/licenses/>.
 */
-describe('ActivityfiltersfieldsetCtrl', function() {
+describe('ActivityfiltersfieldsetCtrl', function () {
 
 	beforeEach(module('unionvmsWeb'));
 
-	var scope,ctrl;
+	var scope, ctrl, mdrCacheServiceSpy, $httpBackend;
 
-	beforeEach(inject(function($httpBackend) {
-        //Mock
-        $httpBackend.whenGET(/usm/).respond();
-        $httpBackend.whenGET(/i18n/).respond();
-        $httpBackend.whenGET(/globals/).respond({data : []});
-    }));
+	beforeEach(function () {
+		mdrCacheServiceSpy = jasmine.createSpyObj('mdrCacheService', ['getCodeList']);
+		module(function ($provide) {
+			$provide.value('mdrCacheService', mdrCacheServiceSpy);
+		});
+		buildMocks();
+	});
 
-    beforeEach(inject(function($rootScope, $controller) {
-      	scope = $rootScope.$new();
-      	ctrl = $controller('ActivityfiltersfieldsetCtrl', {$scope: scope});
+	beforeEach(inject(function ($rootScope, $controller, $injector) {
+		$httpBackend = $injector.get('$httpBackend');
+		$httpBackend.whenGET(/usm/).respond();
+		$httpBackend.whenGET(/i18n/).respond();
+		$httpBackend.whenGET(/globals/).respond({ data: [] });
+
+		scope = $rootScope.$new();
+		ctrl = $controller('ActivityfiltersfieldsetCtrl', { $scope: scope });
+
 		scope.$digest();
-    }));	
+	}));
 
-	it('should load the combo items', inject(function() {
-			expect(scope.reportTypes).not.toBeUndefined();
-			expect(scope.reportTypes.length).not.toEqual(0);
+	function getGearTypes() {
+		return [{ "code": "OTT", "text": "OTT", "desc": "OTTER TWIN TRAWLS" }, { "code": "OT", "text": "OT", "desc": "OTTER TRAWLS (NOT SPECIFIED)" }, { "code": "PT", "text": "PT", "desc": "PAIR TRAWLS (NOT SPECIFIED)" }, { "code": "TX", "text": "TX", "desc": "OTHER TRAWLS (NOT SPECIFIED)" }, { "code": "DRB", "text": "DRB", "desc": "BOAT DREDGES" }, { "code": "DRH", "text": "DRH", "desc": "HAND DREDGES" }, { "code": "LNP", "text": "LNP", "desc": "PORTABLE LIFT NETS" }, { "code": "LNB", "text": "LNB", "desc": "BOAT-OPERATED LIFT NETS" }, { "code": "LNS", "text": "LNS", "desc": "SHORE-OPERATED STATIONARY LIFT NETS" }, { "code": "LN", "text": "LN", "desc": "LIFT NETS (NOT SPECIFIED)" }, { "code": "TBB", "text": "TBB", "desc": "BOTTOM TRAWLS - BEAM TRAWLS" }, { "code": "OTB", "text": "OTB", "desc": "BOTTOM TRAWLS - OTTER TRAWLS" }, { "code": "PTB", "text": "PTB", "desc": "BOTTOM TRAWLS - PAIR TRAWLS" }, { "code": "TBN", "text": "TBN", "desc": "BOTTOM TRAWLS - NEPHROPS TRAWLS" }, { "code": "TBS", "text": "TBS", "desc": "BOTTOM TRAWLS - SHRIMP TRAWLS" }, { "code": "TB", "text": "TB", "desc": "BOTTOM TRAWLS - NOT SPECIFIED" }, { "code": "OTM", "text": "OTM", "desc": "MIDWATER TRAWLS - OTTER TRAWLS" }, { "code": "PTM", "text": "PTM", "desc": "MIDWATER TRAWLS - PAIR TRAWLS" }, { "code": "TMS", "text": "TMS", "desc": "MIDWATER TRAWLS - SHRIMP TRAWLS" }, { "code": "TM", "text": "TM", "desc": "MIDWATER TRAWLS - NOT SPECIFIED" }, { "code": "PS", "text": "PS", "desc": "WITH PURSE LINES (PURSE SEINES)" }, { "code": "PS1", "text": "PS1", "desc": "- ONE BOAT OPERATED PURSE SEINES" }, { "code": "PS2", "text": "PS2", "desc": "- TWO BOATS OPERATED PURSE SEINES" }, { "code": "LA", "text": "LA", "desc": "WITHOUT PURSE LINES (LAMPARA)" }, { "code": "SB", "text": "SB", "desc": "BEACH SEINES" }, { "code": "SV", "text": "SV", "desc": "BOAT OR VESSEL SEINES" }, { "code": "SDN", "text": "SDN", "desc": "- DANISH SEINES" }, { "code": "SSC", "text": "SSC", "desc": "- SCOTTISH SEINES" }, { "code": "SPR", "text": "SPR", "desc": "- PAIR SEINES" }, { "code": "SX", "text": "SX", "desc": "SEINE NETS (NOT SPECIFIED)" }, { "code": "NK", "text": "NK", "desc": "GEAR NOT KNOW OR NOT SPECIFIED" }, { "code": "LLD", "text": "LLD", "desc": "DRIFTING LONGLINES" }, { "code": "LL", "text": "LL", "desc": "LONGLINES (NOT SPECIFIED)" }, { "code": "LTL", "text": "LTL", "desc": "TROLLING LINES" }, { "code": "LX", "text": "LX", "desc": "HOOKS AND LINES (NOT SPECIFIED)" }, { "code": "HAR", "text": "HAR", "desc": "HARPOONS" }, { "code": "HMP", "text": "HMP", "desc": "PUMPS" }, { "code": "HMD", "text": "HMD", "desc": "MECHANIZED DREDGES" }, { "code": "HMX", "text": "HMX", "desc": "HARVESTING MACHINES (NOT SPECIFIED)" }, { "code": "MIS", "text": "MIS", "desc": "MISCELLANEOUS GEAR" }, { "code": "RG", "text": "RG", "desc": "RECREATIONAL FISHING GEAR" }, { "code": "FPN", "text": "FPN", "desc": "STATIONARY UNCOVERED POUND NETS" }, { "code": "FPO", "text": "FPO", "desc": "POTS" }, { "code": "FYK", "text": "FYK", "desc": "FYKE NETS" }, { "code": "FSN", "text": "FSN", "desc": "STOW NETS" }, { "code": "FWR", "text": "FWR", "desc": "BARRIERS, FENCES, WEIRS, ETC." }, { "code": "FAR", "text": "FAR", "desc": "AERIAL TRAPS" }, { "code": "FIX", "text": "FIX", "desc": "TRAPS (NOT SPECIFIED)" }, { "code": "LHP", "text": "LHP", "desc": "HANDLINES AND POLE-LINES (HAND-OPERATED)" }, { "code": "LHM", "text": "LHM", "desc": "HANDLINES AND POLE-LINES (MECHANIZED)" }, { "code": "LLS", "text": "LLS", "desc": "SET LONGLINES" }, { "code": "FCN", "text": "FCN", "desc": "CAST NETS" }, { "code": "FG", "text": "FG", "desc": "FALLING GEAR (NOT SPECIFIED)" }, { "code": "GNS", "text": "GNS", "desc": "SET GILLNETS (ANCHORED)" }, { "code": "GND", "text": "GND", "desc": "DRIFTNETS" }, { "code": "GNC", "text": "GNC", "desc": "ENCIRCLING GILLNETS" }, { "code": "GNF", "text": "GNF", "desc": "FIXED GILLNETS (ON STAKES)" }, { "code": "GTR", "text": "GTR", "desc": "TRAMMEL NETS" }, { "code": "GTN", "text": "GTN", "desc": "COMBINED GILLNETS-TRAMMEL NETS" }, { "code": "GEN", "text": "GEN", "desc": "GILLNETS AND ENTANGLING NETS (NOT SPECIFIED)" }, { "code": "GN", "text": "GN", "desc": "GILLNETS (NOT SPECIFIED)" }];
+	}
+	function getReportTypes() {
+		return [{
+			code: "DECLARATION",
+			description: "DECLARATION"
+		},
+		{
+			code: "NOTIFICATION",
+			description: "NOTIFICATION"
+		}];
+	}
+	function getActivityType() {
+		return [{ "code": "JOINT_FISHING_OPERATION", "text": "JFO", "desc": "A FISHING OPERATION WITH MORE THAN ONE VESSEL INVOLVED" }, { "code": "DEPARTURE", "text": "DEP", "desc": "DEPARTURE FROM A LOCATION." }, { "code": "ARRIVAL", "text": "ARR", "desc": "ARRIVAL IN A LOCATION" }, { "code": "AREA_ENTRY", "text": "ENTRY", "desc": "ENTRY INTO AN AREA" }, { "code": "AREA_EXIT", "text": "EXIT", "desc": "EXIT FROM AN AREA" }, { "code": "FISHING_OPERATION", "text": "FOP", "desc": "ACTIVITY IN CONNECTION WITH SEARCHING FOR FISH, SHOOTING, TOWING AND HAULING OF ACTIVE GEARS, SETTING, SOAKING, REMOVING/RESETTING OF PASSIVE GEARS AND REMOVAL OF ANY CATCH FROM THE GEAR, KEEP NETS OR FROM A TRANSPORT CAGE TO FATTENING/FARMING CAGES" }, { "code": "LANDING", "text": "LAN", "desc": "THE UNLOADING OF CATCHES OR PART THEREOF IN A PORT OR LANDING PLACE ON LAND" }, { "code": "DISCARD", "text": "DIS", "desc": "THE UNLOADING OF CATCHES OR PART THEREOF WHILE AT SEA" }, { "code": "TRANSHIPMENT", "text": "TRA", "desc": "THE UNLOADING OF CATCHES OR PART THEREOF FROM ONBOARD THE VESSEL TO ANOTHER VESSEL OR THE LOADING OF CATCHES ON BOARD FROM ANOTHER VESSEL" }, { "code": "RELOCATION", "text": "RLC", "desc": "OPERATION WHERE THE CATCH OR PART THEREOF IS TRANSFERRED OR MOVED FROM SHARED FISHING GEAR TO A VESSEL OR FROM A VESSEL’S HOLD OR ITS FISHING GEAR TO A KEEP NET, CONTAINER OR CAGE OUTSIDE THE VESSEL IN WHICH THE LIVE CATCH IS KEPT UNTIL LANDING." }];
+	}
 
-			expect(scope.activityTypes).not.toBeUndefined();
-			expect(scope.activityTypes.length).not.toEqual(0);
+	function buildMocks() {
+		mdrCacheServiceSpy.getCodeList.andCallFake(function (param) {
+			if (param === 'GEAR_TYPE') {
+				return {
+					then: function (callback) {
+						return callback(getGearTypes());
+					}
+				};
+			} else if (param === 'FLUX_FA_REPORT_TYPE') {
+				return {
+					then: function (callback) {
+						return callback(getReportTypes());
+					}
+				};
+			} else {
+				return {
+					then: function (callback) {
+						return callback(getActivityType());
+					}
+				};
+			}
+		});
+	}
 
-			/*expect(scope.ports).not.toBeUndefined();
-			expect(scope.ports.length).not.toEqual(0);*/
-
-			expect(scope.gearTypes).not.toBeUndefined();
-			expect(scope.gearTypes.length).not.toEqual(0);
-
-			/*expect(scope.species).not.toBeUndefined();
-			expect(scope.species.length).not.toEqual(0);*/
-
-			expect(scope.weightUnits).not.toBeUndefined();
-			expect(scope.weightUnits.length).not.toEqual(0);
+	it('should load the combo items', inject(function () {
+		expect(mdrCacheServiceSpy.getCodeList.callCount).toBe(3);
 	}));
 
 });
--- app/partial/spatial/reportsPanel/reportForm/activityFiltersFieldset/activityFiltersFieldset.js
+++ app/partial/spatial/reportsPanel/reportForm/activityFiltersFieldset/activityFiltersFieldset.js
@@ -14,49 +14,33 @@ copy of the GNU General Public License along with the IFDM Suite. If not, see <h
  * @ngdoc controller
  * @name ActivityfiltersfieldsetCtrl
  * @param $scope {Service} controller scope
+ * @param mdrCacheService {Service} The mdr code lists cache service <p>{@link unionvmsWeb.mdrCacheService}</p>
+ * @param locale {service} angular locale service
  * @description
  *  The controller for the activity filters fieldset
  */
-angular.module('unionvmsWeb').controller('ActivityfiltersfieldsetCtrl',function($scope){
+angular.module('unionvmsWeb').controller('ActivityfiltersfieldsetCtrl', function ($scope, locale, mdrCacheService) {
+
+    $scope.codeLists = {
+        comChannels: null,
+        purposeCodes: null,
+        reportTypes: null,
+        gearTypes: null,
+        activityTypes: null,
+        weightUnits: [{ code: 'kg', text: 'Kg' }, { code: 'TNE', text: 'Ton' }]
+    };
+
+    $scope.visibleCombos = {
+        reportType: true,
+        activityType: true,
+        gearType: true
+    };
 
     //TODO replace the mocks when the REST service is ready
-    $scope.reportTypes = [{
-        code: 'NOTIFICATION', text: 'Notification'
-    },{
-        code: 'DECLARATION', text: 'Declaration'
-    }];
-
-    //TODO replace the mocks when the REST service is ready
-    $scope.activityTypes = [{
-        code: 'DEPARTURE', text: 'Departure'
-    },{
-        code: 'LANDING', text: 'Landing'
-    },{
-        code: 'ARRIVAL', text: 'Arrival'
-    },{
-        code: 'FISHING_OPERATION', text: 'Fishing operation'
-    }];
-
-    //TODO replace the mocks when the REST service is ready
-    /*$scope.ports = [{
-        code: 'PORT1', text: 'PORT1'
-    },{
-        code: 'PORT2', text: 'PORT2'
-    },{
-        code: 'PORT3', text: 'PORT3'
-    },{
-        code: 'PORT4', text: 'PORT4'
-    }];*/
-
-    //TODO replace the mocks when the REST service is ready
-    $scope.gearTypes = [{
-        code: 'GNS', text: 'Set gillnets (anchored)'
-    },{
-        code: 'GND', text: 'Driftnets'
-    },{
-        code: 'GNC', text: 'Encircling gillnets'
-    },{
-        code: 'GTR', text: 'Combined gillnets-trammel nets'
+    $scope.weightUnits = [{
+        code: 'kg', text: 'Kg'
+    }, {
+        code: 'ton', text: 'Ton'
     }];
 
     //TODO replace the mocks when the REST service is ready
@@ -71,10 +55,138 @@ angular.module('unionvmsWeb').controller('ActivityfiltersfieldsetCtrl',function(
     }];*/
 
     //TODO replace the mocks when the REST service is ready
-    $scope.weightUnits = [{
-        code: 'kg', text: 'Kg'
+    /*$scope.ports = [{
+        code: 'PORT1', text: 'PORT1'
     },{
-        code: 'ton', text: 'Ton'
-    }];
+        code: 'PORT2', text: 'PORT2'
+    },{
+        code: 'PORT3', text: 'PORT3'
+    },{
+        code: 'PORT4', text: 'PORT4'
+    }];*/
 
+    /**
+     * Get the data for all comboboxes used in the the advanced search form
+     * 
+     * @memberof ActivityfiltersfieldsetCtrl
+     * @private
+     */
+    function getComboboxData() {
+        var lists = ['reportTypes', 'gearTypes', 'activityTypes'];
+        angular.forEach(lists, function (list) {
+            var fnName = 'get' + list.substring(0, 1).toUpperCase() + list.substring(1);
+            $scope[fnName]();
+        });
+    }
+
+    /**
+     * Get all activity types from MDR
+     * 
+     * @memberof ActivityfiltersfieldsetCtrl
+     * @public
+     * @alias getActivityTypes
+     * @returns {Array} An array with all activity types
+     */
+    $scope.getActivityTypes = function () {
+        $scope.codeLists.activityTypes = [];
+        mdrCacheService.getCodeList('FLUX_FA_TYPE').then(function (response) {
+            var suportedCodes = ['DEPARTURE', 'ARRIVAL', 'AREA_ENTRY', 'AREA_EXIT', 'FISHING_OPERATION', 'LANDING', 'DISCARD', 'TRANSHIPMENT', 'RELOCATION', 'JOINED_FISHING_OPERATION'];
+            $scope.codeLists.activityTypes = convertCodelistToCombolist(response, true, true, suportedCodes);
+        }, function (error) {
+            $scope.actServ.setAlert(true, 'activity.activity_error_getting_code_lists');
+            $scope.visibleCombos.activityType = false;
+        });
+    };
+
+    /**
+       * Get all report types from MDR
+       * 
+       * @memberof ActivityfiltersfieldsetCtrl
+       * @public
+       * @alias getReportTypes
+       * @returns {Array} An array with all report types
+       */
+    $scope.getReportTypes = function () {
+        $scope.codeLists.reportTypes = [];
+        mdrCacheService.getCodeList('FLUX_FA_REPORT_TYPE').then(function (response) {
+            $scope.codeLists.reportTypes = convertCodelistToCombolist(response, false, false);
+        }, function (error) {
+            $scope.actServ.setAlert(true, 'activity.activity_error_getting_code_lists');
+            $scope.visibleCombos.reportType = false;
+        });
+    };
+
+
+    /**
+     * Get all gear types from MDR
+     * 
+     * @memberof ActivityfiltersfieldsetCtrl
+     * @public
+     * @alias getGearTypes
+     * @returns {Array} An array with all gear types
+     */
+    $scope.getGearTypes = function () {
+        $scope.codeLists.gearTypes = [];
+        mdrCacheService.getCodeList('GEAR_TYPE').then(function (response) {
+            $scope.codeLists.gearTypes = convertCodelistToCombolist(response, true, false);
+        }, function (error) {
+            $scope.actServ.setAlert(true, 'activity.activity_error_getting_code_lists');
+            $scope.visibleCombos.gearType = false;
+        });
+    };
+
+    /**
+     * Convert code lists array into combobox list array
+     * 
+     * @memberof ActivityfiltersfieldsetCtrl
+     * @private
+     * @param {Array} data - The input data array
+     * @param {Boolean} withTooltip - True if the item text and tooltip description should be different
+     * @parm {Boolean} useAbbreviations - Whether the item text should be fetched from the abbreviations lang file or not
+     * @param {Array} [suportedCodes] - An array containing the supported codes. This param is optional
+     * @returns {Array} An array suitable for combobox use
+     */
+    function convertCodelistToCombolist(data, withTooltip, useAbbreviations, suportedCodes) {
+        var comboList = [];
+        angular.forEach(data, function (item) {
+            if (item.code === 'JOINED_FISHING_OPERATION') {
+                item.code = 'JOINT_FISHING_OPERATION';
+            }
+            var rec = {
+                code: item.code,
+                text: item.description
+            };
+            if (withTooltip) {
+                if (useAbbreviations) {
+                    rec.text = locale.getString('abbreviations.activity_' + item.code);
+                } else {
+                    rec.text = item.code;
+                }
+
+                rec.desc = item.description;
+            }
+
+            if (angular.isDefined(suportedCodes)) {
+                if (_.indexOf(suportedCodes, item.code) !== -1 || (item.code === 'JOINT_FISHING_OPERATION' && _.indexOf(suportedCodes, 'JOINED_FISHING_OPERATION') !== -1)) {
+                    comboList.push(rec);
+                }
+            } else {
+                comboList.push(rec);
+            }
+        });
+
+        return comboList;
+    }
+
+    /**
+     * Initialization function
+     * 
+     * @memberof ActivityCtrl
+     * @private
+     */
+    function init() {
+        getComboboxData();
+    }
+
+    init();
 });
