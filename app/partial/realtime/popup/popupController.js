297e9f33cf9dc959466130fac4a977a874373dd4
jonnym2018 <jonas.nyman@havochvatten.se>
2019-02-01 17:10:45 +0100

    set flag state and asset id.

--- app/partial/realtime/popup/popupController.js
+++ app/partial/realtime/popup/popupController.js
@@ -9,22 +9,6 @@ angular.module('unionvmsWeb').controller('PopupCtrl', function($scope, $rootScop
         let positionId = $scope.data.position.guid;
         console.log('show track for :', positionId);
         microMovementRestService.getTrackByMovementId(positionId).then((trackData) => {
-                // track data is in string format, positions in an array, send these back
-                // to draw segments.
-                /*
-                let coordData = [];
-                let wktString = trackData.wkt;
-
-                if (angular.isDefined(wktString) && wktString.indexOf("LINESTRING") !== -1) {
-                    wktString = wktString.replace('LINESTRING (','').replace(')', '');
-                    let coords = wktString.split(', ');
-                    for (var i = 0;i < coords.length; i++) {
-                        let loglat = coords[i].split(' ');
-                        coordData.push(new Array(parseFloat(loglat[0]), parseFloat(loglat[1])));
-                    }
-
-                }
-                */
                 let data = {
                     data: $scope.data,
                     wkt: trackData.wkt
