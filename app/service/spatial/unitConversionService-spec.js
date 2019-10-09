14afc86ca6f41c624abe2d47f751ae9922162d60
Nikos Paraskevopoulos <Nikolaos.PARASKEVOPOULOS@sword-group.com>
2019-10-09 12:06:11 +0300

    [UNIONVMS-4290] Specify time units in unitConversionService.js#timeToHuman()

--- app/service/spatial/unitConversionService-spec.js
+++ app/service/spatial/unitConversionService-spec.js
@@ -9,14 +9,22 @@ the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the impl
 FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a
 copy of the GNU General Public License along with the IFDM Suite. If not, see <http://www.gnu.org/licenses/>.
 */
-describe('unitConversionService', function() {
+describe('The unitConversionService', function() {
+    var sut;
 
-  beforeEach(module('unionvmsWeb'));
+    beforeEach(module('unionvmsWeb'));
 
-  it('should ...', inject(function(unitConversionService) {
+    beforeEach(inject(function(unitConversionService) {
+        sut = unitConversionService;
+    }));
 
-	//expect(unitConversionService.doSomething()).toEqual('something');
+    describe('when converting duration', function() {
+        it('converts a duration to human-readable form, assuming seconds as the default unit', function() {
+            expect(sut.duration.timeToHuman(3665)).toBe('1h 1m 5s');
+        });
 
-  }));
-
+        it('converts a duration to human-readable form, taking into account units in Moment format', function() {
+            expect(sut.duration.timeToHuman(90065000, 'milliseconds')).toBe('1d 1h 1m 5s');
+        });
+    });
 });
