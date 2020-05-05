cb33c5ceefe657950996209ed12018e486eba394
Chara Makri <chara.makri@sword-group.com>
2020-05-05 10:26:52 +0300

    [UNIONVMS-4487] Work on table component and add to selected areas list

--- unionvms-web-2/src/angular/app/features/areas/area-selection/area-selection-table/area-selection-table.component.ts
+++ unionvms-web-2/src/angular/app/features/areas/area-selection/area-selection-table/area-selection-table.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FeaturesService } from 'app/features/features.service';
+import { ColumnMode } from '@swimlane/ngx-datatable';
+import { faCheck  } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-area-selection-table',
@@ -7,9 +10,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AreaSelectionTableComponent implements OnInit {
 
-  constructor() { }
+  @Input() mode;
+  @Input() areaType?;
+  @Output() selectArea = new EventEmitter<any>();
+  results: [];
+  ColumnMode = ColumnMode;
+  loadingIndicator = true;
+  faCheck = faCheck;
+
+
+  constructor(private featuresService: FeaturesService) { }
 
   ngOnInit(): void {
+  }
+
+  async onSubmit(form) {
+    // TODO: refactor
+    if (this.mode === 'USER') {
+        try {
+          const result: any = await this.featuresService.getUserAreasByFilter(form);
+          this.results = result.data;
+          this.loadingIndicator = false;
+        } catch (err) {
+
+        }
+    } else {
+      const obj = {
+        filter: form.filter,
+        areaType: this.areaType
+      };
+      try {
+        const result: any = await this.featuresService.getSystemAreasByFilter(obj);
+        this.results = result.data;
+        this.loadingIndicator = false;
+
+      } catch (err) {
+
+      }
+
+    }
+  }
+
+  onSelectArea(row) {
+    debugger;
+    this.selectArea.emit(row);
+
   }
 
 }
