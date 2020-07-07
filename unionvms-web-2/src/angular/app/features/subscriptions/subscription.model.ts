0162722bb87de118bfc8e7f4a441ee77c333ba43
Nikos Paraskevopoulos <Nikolaos.PARASKEVOPOULOS@sword-group.com>
2020-07-07 17:06:57 +0300

    [UNIONVMS-4683] Batch 3: Rearrange the fields, add the trigger in the results

--- unionvms-web-2/src/angular/app/features/subscriptions/subscription.model.ts
+++ unionvms-web-2/src/angular/app/features/subscriptions/subscription.model.ts
@@ -1,12 +1,20 @@
 export interface Subscription {
-    id: number;
-    messageType: string;
-    name: string;
-    description: string;
-    active: boolean;
-    startDate: Date;
-    endDate: Date;
-    organisationName: string;
-    endpointName: string;
-    channelName: string;
-  }
+  id: number;
+  messageType: string;
+  triggerType: string;
+  name: string;
+  description: string;
+  active: boolean;
+  startDate: Date;
+  endDate: Date;
+  organisationName: string;
+  endpointName: string;
+  channelName: string;
+}
+
+export interface SubscriptionListResponseDto {
+  currentPage: number;
+  totalNumberOfPages: number;
+  totalCount: number;
+  subscriptionList: Subscription[];
+}
