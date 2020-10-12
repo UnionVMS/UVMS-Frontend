193cb0385723df9ca4148c178590226734d6e041
PROTOPAPAS Thanasis <thanasis.protopapas@sword-group.com>
2020-10-12 18:39:20 +0300

    [UVMS-4825] Disable the alert switch before a trigger selection

--- unionvms-web-2/src/angular/app/features/subscriptions/subscription-form/subscription-form.component.ts
+++ unionvms-web-2/src/angular/app/features/subscriptions/subscription-form/subscription-form.component.ts
@@ -124,7 +124,7 @@ export class SubscriptionFormComponent implements OnInit, OnDestroy {
           passwordIsPlaceholder: [false],
           isXml: [false]
         }),
-        alert: [false],
+        alert: [{value: false, disabled: true}],
         subscriber: this.fb.group({
           organisationId: [null],
           endpointId: [{value: null, disabled: true}],
