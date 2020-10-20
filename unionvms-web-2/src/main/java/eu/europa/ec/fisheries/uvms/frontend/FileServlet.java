0b1acb4ab2d5eb5e3f24b5e3862658ea428c3388
Nikos Paraskevopoulos <Nikolaos.PARASKEVOPOULOS@sword-group.com>
2020-10-20 19:02:55 +0300

    [UNIONVMS-4748] Adapting to the URL rewriting that happens in DG Mare servers

--- unionvms-web-2/src/main/java/eu/europa/ec/fisheries/uvms/frontend/FileServlet.java
+++ unionvms-web-2/src/main/java/eu/europa/ec/fisheries/uvms/frontend/FileServlet.java
@@ -6,13 +6,31 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.ServletException;
 import java.io.IOException;
+import java.util.Optional;
+import java.util.stream.Stream;
 
 @WebServlet(urlPatterns={
   "/uvms-poc/subscriptions/*"
 })
 public class FileServlet extends HttpServlet {
+
+  private static final String[] FILE_SUFFIXES = new String[] {".png", ".svg", ".woff", ".eot", ".ttf", ".ico", ".html", ".js", ".css"};
+
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-    req.getRequestDispatcher("/uvms-poc/index.html").forward(req, resp);
+    String pathInfo = Optional.ofNullable(req.getPathInfo()).orElse("");
+    if (Stream.of(FILE_SUFFIXES).anyMatch(pathInfo::endsWith)) {
+      int index = pathInfo.lastIndexOf("/assets/");
+      if (index == -1) {
+        index = pathInfo.lastIndexOf("/");
+      }
+      if (index >= 0) {
+        resp.sendRedirect(req.getContextPath() + "/uvms-poc" + pathInfo.substring(index));
+      } else {
+        resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+      }
+    } else {
+      req.getRequestDispatcher("/uvms-poc/index.html").forward(req, resp);
+    }
   }
 }
