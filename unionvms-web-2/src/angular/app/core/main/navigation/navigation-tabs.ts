3e769d4487831de0617844fe6f81813a0fb9eba5
gmanifavas <george.manifavas@sword-group.com>
2020-11-12 18:24:57 +0200

    Header tabs are added according to user permissions.

--- unionvms-web-2/src/angular/app/core/main/navigation/navigation-tabs.ts
+++ unionvms-web-2/src/angular/app/core/main/navigation/navigation-tabs.ts
@@ -4,79 +4,64 @@ export const NAVIGATION_TABS = [
   {
     id: 1,
     name: 'Today',
-    href: `${environment.oldBaseURL}/#/today`,
-    // permissions: [300001]
+    href: `${environment.oldBaseURL}/#/today`
   },
   {
     id: 2,
     name: 'Reports',
     href: `${environment.oldBaseURL}/#/reporting`,
-    // permissions: [300001]
+    permissions: [21] //LIST_REPORTS
   },
   {
     id: 3,
     name: 'Area management',
     href: `${environment.oldBaseURL}/#/areas`,
-    // permissions: [100041]
+    permissions: [100036] //VIEW_AREA_MANAGEMENT_UI
   },
   {
     id: 4,
     name: 'Subscriptions',
     route: '/subscriptions',
-    // permissions: [300001]
+    permissions: [300000] //VIEW_SUBSCRIPTION
   },
   {
     id: 5,
     name: 'Activity',
     href: `${environment.oldBaseURL}/#/activity`,
-    // permissions: [279]
+    permissions: [100026] //ACTIVITY_ALLOWED
   },
   {
     id: 6,
-    name: 'Positions',
-    href: `${environment.oldBaseURL}/#/movement`,
-    // permissions: [300001]
+    name: 'Positions'
   },
   {
     id: 7,
     name: 'Sales',
     href: `${environment.oldBaseURL}/#/sales`,
-    // permissions: [300001]
+    permissions: [300002] //viewSalesReports
   },
   {
     id: 8,
     name: 'Exchange',
     href: `${environment.oldBaseURL}/#/exchange`,
-    // permissions: [300001]
+    permissions: [249] //viewExchange
   },
   {
     id: 9,
-    name: 'Polling',
-    href: `${environment.oldBaseURL}/#/polling/logs`,
-    // permissions: [100034]
+    name: 'Assets',
+    href: `${environment.oldBaseURL}/#/assets`,
+    permissions: [292] //viewVesselsAndMobileTerminals
+  },
+  {
+    id: 10,
+    name: 'Alerts'
   },
   {
     id: 11,
-    name: 'Assets',
-    href: `${environment.oldBaseURL}/#/assets`,
-    // permissions: [300001]
+    name: 'User'
   },
   {
     id: 12,
-    name: 'Alerts',
-    href: `${environment.oldBaseURL}/#/alerts/holdingtable`,
-    // permissions: [300001]
-  },
-  {
-    id: 13,
-    name: 'User',
-    href: `${environment.oldBaseURL}/#/usm/users`,
-    // permissions: [300001]
-  },
-  {
-    id: 14,
-    name: 'Admin',
-    href: `${environment.oldBaseURL}/#/admin/auditlog`,
-    // permissions: [300001]
+    name: 'Admin'
   }
 ];
